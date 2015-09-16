662c651c6fecbcb32fc63323f1d257d60b51bff8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-15 20:29:05 -0400

    Create ex4.rs

---
+++ ex4.rs
@@ -0,0 +1,13 @@
+// Make me compile!
+
+fn something() -> Result<i32, std::num::ParseIntError> {
+    let x:i32 = "3".parse();
+    Ok(x * 4)
+}
+
+fn main() {
+    match something() {
+        Ok(..) => println!("You win!"),
+        Err(e) => println!("Oh no something went wrong: {}", e),
+    }
+}
