253ff9d75c0312f5486e5a5eaa5be12ad65ee696
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-16 09:49:09 -0400

    Create ex5.rs

---
+++ ex5.rs
@@ -0,0 +1,21 @@
+// Make me compile!
+
+enum Reaction<'a> {
+    Sad(&'a str),
+    Happy(&'a str),
+}
+
+fn express(sentiment: Reaction) {
+    match sentiment {
+        Reaction::Sad(s) => println!(":( {}", s),
+        Reaction::Happy(s) => println!(":) {}", s),
+    }
+}
+
+fn main () {
+    let x = Reaction::Happy("It's a great day for Rust!");
+    express(x);
+    express(x);
+    let y = Reaction::Sad("This code doesn't compile yet.");
+    express(y);
+}
