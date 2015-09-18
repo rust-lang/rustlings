9336aed2b94264cd00aab60defcaae462efb8bc0
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-17 22:21:56 -0400

    Add module exercises from jdm! :tada:
    Fixes #9.

---
+++ modules/modules1.rs
@@ -0,0 +1,42 @@
+// Make me compile! Scroll down for hints :)
+
+mod sausage_factory {
+    fn make_sausage() {
+        println!("sausage!");
+    }
+}
+
+fn main() {
+    sausage_factory::make_sausage();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Everything is private in Rust by default-- but there's a keyword we can use
+// to make something public! The compiler error should point to the thing that
+// needs to be public.
---
+++ modules/modules2.rs
@@ -0,0 +1,47 @@
+// Make me compile! Scroll down for hints :)
+
+mod us_presidential_frontrunners {
+    use self::democrats::HILLARY_CLINTON as democrat;
+    use self::republicans::DONALD_TRUMP as republican;
+
+    mod democrats {
+        pub const HILLARY_CLINTON: &'static str = "Hillary Clinton";
+        pub const BERNIE_SANDERS: &'static str = "Bernie Sanders";
+    }
+
+    mod republicans {
+        pub const DONALD_TRUMP: &'static str = "Donald Trump";
+        pub const JEB_BUSH: &'static str = "Jeb Bush";
+    }
+}
+
+fn main() {
+    println!("candidates: {} and {}",
+             us_presidential_frontrunners::democrat,
+             us_presidential_frontrunners::republican);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The us_presidential_frontrunners module is trying to present an external
+// interface (the `democrat` and `republican` constants) that is different than
+// its internal structure (the `democrats` and `republicans` modules and
+// associated constants). It's almost there except for one keyword missing for
+// each constant.
+// One more hint: I wish the compiler error, instead of saying "unresolved name
+// `us_presidential_frontrunners::democrat`", could say  "constant
+// `us_presidential_frontrunners::democrat` is private"!
