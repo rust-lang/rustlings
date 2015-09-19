36a75c733e4bf3f5ae5f375adb2b290c09494481
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-18 20:28:27 -0400

    Just remembered another function one I wanted to do

---
+++ functions/functions5.rs
@@ -0,0 +1,43 @@
+// Make me compile! Scroll down for hints :)
+
+fn main() {
+    let answer = square(3);
+    println!("The answer is {}", answer);
+}
+
+fn square(num: i32) -> i32 {
+    num * num;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is a really common error that can be fixed by removing one character.
+// It happens because Rust distinguishes between expressions and statements: expressions return
+// a value and statements don't. We want to return a value from the `square` function, but it
+// isn't returning one right now...
