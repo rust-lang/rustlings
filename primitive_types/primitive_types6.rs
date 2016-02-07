984f1d70782ee2c7f4c3277c676c459112a165dc
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-02-07 12:34:53 -0500

    Add tuple indexing exercise from @ruipserra!! 👏
    Connects to #29

---
+++ primitive_types/primitive_types6.rs
@@ -0,0 +1,43 @@
+// Use a tuple index to access the second element of `numbers`.
+// You can put this right into the `println!` where the ??? is.
+// Scroll down for hints!
+
+fn main() {
+    let numbers = (1, 2, 3);
+    println!("The second number is {}", ???);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// While you could use a destructuring `let` for the tuple here, try 
+// indexing into it instead, as explained here:
+// http://doc.rust-lang.org/stable/book/primitive-types.html#tuple-indexing
+// Now you have another tool in your toolbox!
