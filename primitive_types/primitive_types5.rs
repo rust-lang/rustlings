bbe1dc59372cdc79993fea68b2f1bec78263681b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-02-07 12:30:08 -0500

    Add tuple exercise from @ruipserra! 😻

---
+++ primitive_types/primitive_types5.rs
@@ -0,0 +1,44 @@
+// Destructure the `cat` tuple so that the println will work.
+// Scroll down for hints!
+
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+    let /* your pattern here */ = cat;
+
+    println!("{} is {} years old.", name, age);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Take a look at the Primitive Types -> Tuples section of the book:
+// http://doc.rust-lang.org/stable/book/primitive-types.html#tuples
+// Particularly the part about "destructuring lets". You'll need to
+// make a pattern to bind `name` and `age` to the appropriate parts
+// of the tuple. You can do it!!
