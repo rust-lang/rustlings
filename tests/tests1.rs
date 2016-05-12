6e71d20086e5f20d6227524049d6e0fc701d85d0
laboon <laboon@gmail.com>
2016-05-12 19:59:44 -0400

    Add directions on running tests

--- tests/tests1.rs
+++ tests/tests1.rs
@@ -1,3 +1,7 @@
+// Tests are important to ensure that your code does what you think it should do.
+// Tests can be run on this file with the following command:
+// rustc --test tests1.rs
+
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Scroll down for hints :)
 
