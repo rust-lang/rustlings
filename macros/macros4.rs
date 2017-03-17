2b70161b4f5961c313439ef8a132c4dfcd597216
QuietMisdreavus <grey@quietmisdreavus.net>
2017-03-17 09:47:45 -0500

    add macros4 example

---
+++ macros/macros4.rs
@@ -0,0 +1,76 @@
+// Make me compile! Scroll down for hints :)
+
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    }
+    ($val:expr) => {
+        println!("Look at this other macro: {}", $val);
+    }
+}
+
+fn main() {
+    my_macro!();
+    my_macro!(7777);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// You only need to add a single character to make this compile.
+
+
+
+
+
+
+
+
+
+// The way macros are written, it wants to see something between each "macro arm", so it can
+// separate them.
