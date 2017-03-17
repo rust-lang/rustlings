248d87a2dd3eec0dba93ef13f7a999940c9d4840
QuietMisdreavus <grey@quietmisdreavus.net>
2017-03-17 09:30:29 -0500

    add macros examples

---
+++ macros/macros3.rs
@@ -0,0 +1,74 @@
+// Make me compile, without taking the macro out of the module! Scroll down for hints :)
+
+mod macros {
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
+fn main() {
+    my_macro!();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// In order to use a macro outside of its module, you need to do something special to the module to
+// lift the macro out into its parent.
+
+
+
+
+
+
+
+
+// The same trick also works on "extern crate" statements for crates that have exported macros, if
+// you've seen any of those around.
