0cd22309a1272ee0abdcd5a874e0df79f20f9931
Robert M Lugg <robert.lugg@gmail.com>
2018-02-19 18:43:33 -0800

    Link to second edition of book

--- primitive_types/primitive_types3.rs
+++ primitive_types/primitive_types3.rs
@@ -40,6 +40,6 @@ fn main() {
 // There's a shorthand to initialize Arrays with a certain size that does not 
 // require you to type in 100 items (but you certainly can if you want!)
 // Check out the Primitive Types -> Arrays section of the book:
-// http://doc.rust-lang.org/stable/book/primitive-types.html#arrays
+// https://doc.rust-lang.org/stable/book/second-edition/ch03-02-data-types.html#arrays
 // Bonus: what are some other things you could have that would return true
 // for `a.len() >= 100`?
