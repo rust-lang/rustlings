2f1e3bc0c7fbc4f6a692ce6315ab4c775a6f52fe
olivia <olivia@fastmail.com>
2018-05-22 21:24:43 +0200

    small fixes

--- src/about_variables.rs
+++ src/about_variables.rs
@@ -16,16 +16,16 @@ pub fn exercise_one() {
     //      ^  ^
     //      |  |
     // What's  The variable
-    //  in it  itself
+    //  in it  name
 }
 
 // Did you get all that? The "let" word basically tells us that we now want to
-// define a variable, and what follows it, the "x" is the name of the variable.
+// define a variable, and what follows it (the "x") is the name of the variable.
 // Each variable has a name, like a label you put on your cardboard box so you
 // don't confuse it with another, similar looking one.
 // The whole "verify!" deal essentially means that Rustlings is checking if you
 // solved the exercise correctly. It compares the first argument with the
-// second, so in this case "0" with "x", where "x" is the _value_ of the variable
+// second, so in this case "0" with "x", where "x" is the *value* of the variable
 // we called "x". When you write "x", you pull out the cardboard box labelled "x"
 // and take out what's inside of it.
 // Speaking of which, what *is* inside of our "x" cardboard box? I don't think it's
