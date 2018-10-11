33565401e29c067383450501f4b67217888e6514
olivia hugger <olivia@fastmail.com>
2018-10-11 14:03:59 +0200

    Update functions5.rs

--- functions/functions5.rs
+++ functions/functions5.rs
@@ -40,7 +40,7 @@ fn square(num: i32) -> i32 {
 
 // This is a really common error that can be fixed by removing one character.
 // It happens because Rust distinguishes between expressions and statements: expressions return
-// a value based on its operand, and statements simply retun a () type which behaves just like `void` in C/C++ language. 
+// a value based on its operand, and statements simply return a () type which behaves just like `void` in C/C++ language. 
 // We want to return a value of `i32` type from the `square` function, but it is returning a `()` type...
 // They are not the same. There are two solusions:
 // 1. Add a `return` ahead of `num * num;`
