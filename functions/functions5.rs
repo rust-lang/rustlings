5045db700a8ed30b9b73d36142c87ba5a0327f96
Chad Dougherty <crd@acm.org>
2018-10-26 16:22:23 -0400

    Update functions5.rs
    simple typo: solusions -> solutions

--- functions/functions5.rs
+++ functions/functions5.rs
@@ -42,6 +42,6 @@ fn square(num: i32) -> i32 {
 // It happens because Rust distinguishes between expressions and statements: expressions return
 // a value based on its operand, and statements simply return a () type which behaves just like `void` in C/C++ language. 
 // We want to return a value of `i32` type from the `square` function, but it is returning a `()` type...
-// They are not the same. There are two solusions:
+// They are not the same. There are two solutions:
 // 1. Add a `return` ahead of `num * num;`
 // 2. remove `;`, make it to be `num * num`
