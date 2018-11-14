6cc16edff0ee30bc4cc482724f3a3579ec65fa6d
olivia <olivia@fastmail.com>
2018-11-14 20:04:48 +0100

    add a fancy ascii header

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,13 @@ fn main() {
       .about("Test")
       .subcommand(SubCommand::with_name("verify").alias("v"))
       .get_matches();
+
+  println!(r#"                 _   _ _                  "#);
+  println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
+  println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
+  println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
+  println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
+  println!(r#"                               |___/      "#);
   
   if let Some(_) = matches.subcommand_matches("verify") {
     compile_only("exercises/ex1.rs");
