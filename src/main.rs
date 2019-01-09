ad26a6d0e61393643a8ad1b99812631d53c7c789
liv <liv@fastmail.com>
2019-01-09 20:08:48 +0100

    revert

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use clap::{App, Arg, SubCommand, crate_version};
+#[macro_use]
+extern crate clap;
+extern crate console;
+extern crate indicatif;
+extern crate syntect;
+extern crate notify;
+
+use clap::{App, Arg, SubCommand};
 use console::{style, Emoji};
 use indicatif::ProgressBar;
 use syntect::easy::HighlightFile;
 use syntect::parsing::SyntaxSet;
 use syntect::highlighting::{ThemeSet, Style};
-use syntect::util::{as_24_bit_terminal_escaped};
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 use std::fs::remove_file;
 use std::io::BufRead;
 use std::process::Command;
+use std::fs::read_to_string;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use notify::DebouncedEvent;
