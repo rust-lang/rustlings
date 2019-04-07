4fa79ee02ff0c07c4670166e17b879d809569539
Chris Pearce <christopher.james.pearce@gmail.com>
2019-04-07 17:12:03 +0100

    Extract command builders into util

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,29 @@
 use std::fs::remove_file;
+use std::process::{Command, Output};
+
+const RUSTC_COLOR_ARGS: &[&str] = &["--color", "always"];
+
+pub fn compile_test_cmd(filename: &str) -> Output {
+    Command::new("rustc")
+        .args(&["--test", filename, "-o", "temp"])
+        .args(RUSTC_COLOR_ARGS)
+        .output()
+        .expect("failed to compile exercise")
+}
+
+pub fn compile_cmd(filename: &str) -> Output {
+    Command::new("rustc")
+        .args(&[filename, "-o", "temp"])
+        .args(RUSTC_COLOR_ARGS)
+        .output()
+        .expect("failed to compile exercise")
+}
+
+pub fn run_cmd() -> Output {
+    Command::new("./temp")
+        .output()
+        .expect("failed to run exercise")
+}
 
 pub fn clean() {
     let _ignored = remove_file("temp");
