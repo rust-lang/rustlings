8cc68ff891753032a72e7e5d4f5a0dbed8bf2ac7
Ivica Šimić <simicivica10@gmail.com>
2019-07-05 13:14:32 +0200

    variables exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -2,7 +2,7 @@
 // Make me compile! Scroll down for hints :)
 
 fn main() {
-    let x;
+    let x = 5;
     if x == 10 {
         println!("Ten!");
     } else {
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -2,7 +2,7 @@
 // Make me compile! Scroll down for hints :)
 
 fn main() {
-    let x: i32;
+    let x: i32 = 5;
     println!("Number {}", x);
 }
 
