3ee889c0509c79e40905ac3fde0f59c5d3a4765e
Paul Czeresko <p.czeresko.3@gmail.com>
2019-07-20 11:43:40 -0400

    Complete if section

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -7,6 +7,7 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // - another function call
     // - additional variables
     // Scroll down for hints.
+    if a > b { a } else { b }
 }
 
 // Don't mind this for now :)
