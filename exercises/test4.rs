9521266b58534e97d26148bb6a5de8e03da56eb6
Paul Czeresko <p.czeresko.3@gmail.com>
2019-07-22 21:01:15 -0400

    Complete test4

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,6 +5,13 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+#[macro_export]
+macro_rules! my_macro {
+    ($x:expr) => {{
+        format!("Hello {}", $x)
+    }};
+}
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
