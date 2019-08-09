e3931718fb7a4355f6fb99a297157df9c03ba694
Niklas Anderson <nkanders@gmail.com>
2019-08-09 07:36:39 -0700

    chore(iterators2): Add exercise instructions

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -1,7 +1,7 @@
 // iterators2.rs
 // In this module, you'll learn some of unique advantages that iterators can offer
 // Step 1. Complete the `capitalize_first` function to pass the first two cases
-// Step 2. Apply the `capitalize_first` function to a vector of strings, ensuring that it
+// Step 2. Apply the `capitalize_first` function to a vector of strings, ensuring that it returns a vector of strings as well
 // Step 3. Apply the `capitalize_first` function again to a list, but try and ensure it returns a single string
 // As always, there are hints below!
 
