dc15032112fc485226a573a18139e5ce928b1755
Thomas Holloway <nyxtom@gmail.com>
2019-10-28 22:49:49 -0500

    feat: Add enums exercises

---
+++ exercises/enums/enums1.rs
@@ -0,0 +1,42 @@
+// enums1.rs
+// Make me compile! Scroll down for hints!
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Hint: The declaration of the enumeration type has not been defined yet.
