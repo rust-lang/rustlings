79a569422c8309cfc9e4aed25bf4ab3b3859996b
marisa <mokou@posteo.de>
2019-10-29 20:53:41 +0100

    fix: Rewrite test1 logic

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -4,7 +4,7 @@
 // - Functions
 
 // Mary is buying apples. One apple usually costs 2 dollars, but if you buy
-// 40 or more at once, each apple only costs 1! Write a function that calculates
+// more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
@@ -13,9 +13,9 @@
 // Don't modify this function!
 #[test]
 fn verify_test() {
-    let price1 = calculate_price(55);
-    let price2 = calculate_price(40);
+    let price1 = calculate_apple_price(35);
+    let price2 = calculate_apple_price(65);
 
-    assert_eq!(55, price1);
-    assert_eq!(80, price2);
+    assert_eq!(70, price1);
+    assert_eq!(65, price2);
 }
