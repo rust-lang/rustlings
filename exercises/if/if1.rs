ad03d180c9311c0093e56a3531eec1a9a70cdb45
marisa <mokou@posteo.de>
2019-11-11 13:34:21 +0100

    fix(if1): Remove `return` reference
    This closes #153.

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -3,7 +3,6 @@
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
-    // - return
     // - another function call
     // - additional variables
     // Scroll down for hints.
