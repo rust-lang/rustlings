fda5a47069e0954f16a04e8e50945e03becb71a5
Chris Keschnat <chris@rivered.de>
2019-11-12 17:27:02 +0100

    fix(variables1): Correct wrong word in comment

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -4,7 +4,7 @@
 // About this `I AM NOT DONE` thing:
 // We sometimes encourage you to keep trying things on a given exercise,
 // even after you already figured it out. If you got everything working and
-// feel ready for the next exercise, you the `I AM NOT DONE` comment below.
+// feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
 
 // I AM NOT DONE
 
