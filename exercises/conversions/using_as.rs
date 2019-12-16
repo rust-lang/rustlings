0c85dc1193978b5165491b99cc4922caf8d14a65
Abdou Seck <djily02016@gmail.com>
2019-12-16 08:34:30 -0500

    feat: Add type conversion and parsing exercises

---
+++ exercises/conversions/using_as.rs
@@ -0,0 +1,16 @@
+// Type casting in Rust is done via the usage of the `as` operator.
+// Please note that the `as` operator is not only used when type casting.
+// It also helps with renaming imports.
+
+// The goal is to make sure that the division does not fail to compile
+fn average(values: &[f64]) -> f64 {
+    let total = values
+        .iter()
+        .fold(0.0, |a, b| a + b);
+    total / values.len()
+}
+
+fn main() {
+    let values = [3.5, 0.3, 13.0, 11.7];
+    println!("{}", average(&values));
+}
