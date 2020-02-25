a45486f2d05ce1081fab5709f629ae765368509b
Stig Johan Berggren <stigjb@gmail.com>
2020-02-25 14:18:31 +0100

    Add a second test case

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -12,8 +12,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_my_macro() {
+    fn test_my_macro_world() {
         assert_eq!(my_macro!("world!"), "Hello world!");
     }
-}
 
+    #[test]
+    fn test_my_macro_goodbye() {
+        assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+    }
+}
