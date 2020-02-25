b559cdd73f32c0d0cfc1feda39f82b3e3583df17
sjmann <sjmann91@gmail.com>
2020-02-25 09:48:50 +0000

    added traits exercises

---
+++ exercises/traits/traits1.rs
@@ -0,0 +1,44 @@
+// traits1.rs
+// Time to implement some traits!
+// 
+// Your task is to implement the trait
+// `AppendBar' for the type `String'.
+// 
+// The trait AppendBar has only one function,
+// which appends "Bar" to any object
+// implementing this trait.
+
+// I AM NOT DONE
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+impl AppendBar for String {
+    //Add your code here
+
+}
+
+fn main() {
+    let s = String::from("Foo");
+    let s = s.append_bar();
+    println!("s: {}", s);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_FooBar() {
+        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+    }
+
+    #[test]
+    fn is_BarBar() {
+        assert_eq!(
+            String::from("").append_bar().append_bar(),
+            String::from("BarBar")
+        );
+    }
+
+}
