2ac1b39196b5af6dde3339e67413881bd371f733
Manuel Gil <manugildev@gmail.com>
2020-03-04 09:30:32 +0000

    Solve functions

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -4,18 +4,16 @@
 // This store is having a sale where if the price is an even number, you get
 // 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
 
-// I AM NOT DONE
-
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> i32 {
     if is_even(price) {
-        price - 10
+        return price - 10;
     } else {
-        price - 3
+        return price - 3;
     }
 }
 
