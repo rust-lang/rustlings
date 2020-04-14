3868596e19fa00f5d7ac801a8bdb3f37e02e6be8
Said Aspen <mail@saidaspen.se>
2020-04-14 08:58:37 +0200

    feat: add exercise variables6, const keyword

---
+++ exercises/variables/variables6.rs
@@ -0,0 +1,9 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+// I AM NOT DONE
+
+const NUMBER = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
