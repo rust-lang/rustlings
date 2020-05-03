41f989135d79eba4602d4fde828698acf9a9f235
apatniv <apatniv@gmail.com>
2020-05-02 20:41:11 -0400

    Review Comments: Add other test cases

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -15,7 +15,8 @@ struct Person {
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
 // 3. Extract the first element from the split operation and use it as the name
-// 4. Extract the other element from the split operation and parse it into a `usize` as the age
+// 4. If the name is empty, then return an error
+// 5. Extract the other element from the split operation and parse it into a `usize` as the age
 // If while parsing the age, something goes wrong, then return an error
 // Otherwise, then return a Result of a Person object
 impl FromStr for Person {
@@ -48,6 +49,37 @@ mod tests {
     #[test]
     #[should_panic]
     fn missing_age() {
+        "John,".parse::<Person>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_age() {
+        "John,twenty".parse::<Person>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn missing_comma_and_age() {
         "John".parse::<Person>().unwrap();
     }
+
+    #[test]
+    #[should_panic]
+    fn missing_name() {
+        ",1".parse::<Person>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn missing_name_and_age() {
+        ",".parse::<Person>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn missing_name_and_invalid_age() {
+        ",one".parse::<Person>().unwrap();
+    }
+
 }
