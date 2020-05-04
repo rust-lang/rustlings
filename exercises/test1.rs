15010f37b01811f7ac4b384159aae91039077e4c
zhihanz <zhihanz@usc.edu>
2020-05-03 20:09:46 -0700

    variables if function test1

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -7,7 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
@@ -20,4 +19,7 @@ fn verify_test() {
 
     assert_eq!(70, price1);
     assert_eq!(65, price2);
+}
+fn calculate_apple_price(number : i32) -> i32{
+    if number > 40 {number * 1} else {number * 2}
 }
