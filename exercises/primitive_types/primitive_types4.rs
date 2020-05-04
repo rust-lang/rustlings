9c559d9965443ee8605ff85543c67f8a45fdeebe
zhihanz <zhihanz@usc.edu>
2020-05-03 21:31:41 -0700

    string, primitive type, struct

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,13 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
-
-    assert_eq!([2, 3, 4], nice_slice)
+    let nice_slice = &a[1..a.len() - 1];
+//https://doc.rust-lang.org/book/ch15-02-deref.html
+// implement a deref trait 
+    assert_eq!([2, 3, 4], *nice_slice)
 }
