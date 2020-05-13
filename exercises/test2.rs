d6c0a688e6a96f93ad60d540d4b326f342fc0d45
Gaurang Tandon <1gaurangtandon@gmail.com>
2020-05-13 16:08:14 +0530

    fix(test2): name of type String and &str (#394)

--- exercises/test2.rs
+++ exercises/test2.rs
@@ -2,7 +2,7 @@
 // This is a test for the following sections:
 // - Strings
 
-// Ok, here are a bunch of values-- some are `Strings`, some are `&strs`. Your
+// Ok, here are a bunch of values-- some are `String`s, some are `&str`s. Your
 // task is to call one of these two functions on each value depending on what
 // you think each value is. That is, add either `string_slice` or `string`
 // before the parentheses on each line. If you're right, it will compile!
