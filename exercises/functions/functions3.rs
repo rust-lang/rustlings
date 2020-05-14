6f5a483d360948756512d94dc0c26cb87d12d5b5
Yuhua Mai <yuhua@urbandoor.com>
2020-05-14 00:04:12 -0700

    Exercises

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,10 +1,8 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(5);
 }
 
 fn call_me(num: i32) {
