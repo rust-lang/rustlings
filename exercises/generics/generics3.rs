0babb9faf70eadefb51ba21eeb56dfe5cb4362bd
tab <dearhange@126.com>
2020-05-17 00:59:07 +0800

    update again

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -1,19 +1,19 @@
 // An imaginary magical school has a new report card generation system written in rust!
-// Currently the system only supports creating report cards where the student's grade 
+// Currently the system only supports creating report cards where the student's grade
 // is represented numerically (e.g. 1.0 -> 5.5). However, the school also issues alphabetical grades
 // (A+ -> F-) and needs to be able to print both types of report card!
 
 // Make the necessary code changes to support alphabetical report cards, thereby making the second
 // test pass.
+use std::fmt::Display;
 
-// I AM NOT DONE
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T:Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
     }
@@ -26,8 +26,8 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1, 
-            student_name: "Tom Wriggle".to_string(), 
+            grade: 2.1,
+            student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
         assert_eq!(report_card.print(), "Tom Wriggle (12) - achieved a grade of 2.1");
@@ -37,8 +37,8 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1, 
-            student_name: "Gary Plotter".to_string(), 
+            grade: "A+",
+            student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
         assert_eq!(report_card.print(), "Gary Plotter (11) - achieved a grade of A+");
--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint iterators3` to get some hints!
 // Have fun :-)
 
-// I AM NOT DONE
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
     NotDivisible(NotDivisibleError),
@@ -70,12 +68,11 @@ mod tests {
     }
 
     // Iterator exercises using your `divide` function
-    /*
     #[test]
     fn result_with_list() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = division_results.collect::<Result<Vec<_>, _>>();
         assert_eq!(format!("{:?}", x), "Ok([1, 11, 1426, 3])");
     }
 
@@ -83,8 +80,7 @@ mod tests {
     fn list_of_results() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = division_results.collect::<Vec<_>>();
         assert_eq!(format!("{:?}", x), "[Ok(1), Ok(11), Ok(1426), Ok(3)]");
     }
-    */
 }
