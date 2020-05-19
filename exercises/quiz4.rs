e7d687f27c7881fe2ba7528cf8c3faf81c27f5dd
Sanjay K <sanjaykdragon@gmail.com>
2020-05-19 12:14:09 -0400

    fixed naming related issues

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -1,9 +1,9 @@
-// test4.rs
-// This test covers the sections:
+// quiz4.rs
+// This quiz covers the sections:
 // - Modules
 // - Macros
 
-// Write a macro that passes the test! No hints this time, you can do it!
+// Write a macro that passes the quiz! No hints this time, you can do it!
 
 // I AM NOT DONE
 
