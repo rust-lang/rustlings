307252e9aeab91ac60af71a0609270de2434b7b3
millefalcon <hanish0019@gmail.com>
2020-06-08 07:51:34 -0400

    Added  example to show the AsRef working

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -36,4 +36,16 @@ mod tests {
         let s = "Cafe au lait";
         assert_eq!(char_counter(s), byte_counter(s));
     }
+
+    #[test]
+    fn different_counts_using_string() {
+        let s = String::from("Café au lait");
+        assert_ne!(char_counter(s.clone()), byte_counter(s));
+    }
+
+    #[test]
+    fn same_counts_using_string() {
+        let s = String::from("Cafe au lait");
+        assert_eq!(char_counter(s.clone()), byte_counter(s));
+    }
 }
