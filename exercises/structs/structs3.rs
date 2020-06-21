c5a563d1d342d5f6bd88dacf9fec9ff22a4f59f7
lukaszKielar <kielar.lukasz@hotmail.com>
2020-06-21 20:58:40 +0200

    finish structs exercises

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,30 +3,33 @@
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     from: String,
     to: String,
-    weight: f32
+    weight: f32,
 }
 
 impl Package {
     fn new(from: String, to: String, weight: f32) -> Package {
         if weight <= 0.0 {
-            // Something goes here...
+            let msg = format!("Weight must be greater than 0, {} specified", weight);
+            panic!(msg)
         } else {
-            return Package {from, to, weight};
+            return Package { from, to, weight };
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.from == self.to {
+            false
+        } else {
+            true
+        }
     }
 
-    fn get_fees(&self, cost_per_kg: f32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cost_per_kg: f32) -> f32 {
+        self.weight * cost_per_kg
     }
 }
 
@@ -47,7 +50,6 @@ mod tests {
     fn create_international_package() {
         let country_from = String::from("Spain");
         let country_to = String::from("Russia");
-        
         let package = Package::new(country_from, country_to, 1.2);
 
         assert!(package.is_international());
@@ -58,10 +60,8 @@ mod tests {
         let country_from = String::from("Spain");
         let country_to = String::from("Spain");
 
-        let country_fee = ???;
-        
+        let country_fee = 8.;
         let package = Package::new(country_from, country_to, 22.0);
-        
         assert_eq!(package.get_fees(country_fee), 176.0);
     }
 }
