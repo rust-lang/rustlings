3286c5ec19ea5fb7ded81d047da5f8594108a490
Ryan McQuen <rpcm@linux.com>
2020-09-07 10:09:27 -0700

    fix(using_as): Add test so that proper type is returned. (#512)

--- exercises/conversions/using_as.rs
+++ exercises/conversions/using_as.rs
@@ -3,6 +3,7 @@
 // It also helps with renaming imports.
 //
 // The goal is to make sure that the division does not fail to compile
+// and returns the proper type.
 
 // I AM NOT DONE
 
@@ -14,4 +15,14 @@ fn average(values: &[f64]) -> f64 {
 fn main() {
     let values = [3.5, 0.3, 13.0, 11.7];
     println!("{}", average(&values));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_proper_type_and_value() {
+        assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]), 7.125);
+    }
 }
