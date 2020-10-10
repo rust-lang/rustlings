1b2ece2186e8606f3aab6a91d973f4927c4bf837
Axel Viala <axel.viala@darnuria.eu>
2020-10-10 13:59:29 +0200

    Rewording according to fmoko suggestion.

--- exercises/primitive_types/primitive_types6.rs
+++ exercises/primitive_types/primitive_types6.rs
@@ -8,9 +8,9 @@
 #[test]
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
-    /// Replace below ??? by tuple indexing syntax.
+    /// Replace below ??? with the tuple indexing syntax.
     let second = ???;
 
     assert_eq!(2, second
-        "This is not the 2nd number of the tuple numbers")
+        "This is not the 2nd number in the tuple!")
 }
