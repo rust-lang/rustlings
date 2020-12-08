30644c9a062b825c0ea89435dc59f0cad86b110e
Peter N <spacey-github.com@ssr.com>
2020-12-08 06:08:25 -0300

    fix: gives a bit more context to magic number

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -1,7 +1,8 @@
 // threads1.rs
 // Make this compile! Execute `rustlings hint threads1` for hints :)
 // The idea is the thread spawned on line 21 is completing jobs while the main thread is
-// monitoring progress until 10 jobs are completed. If you see 6 lines
+// monitoring progress until 10 jobs are completed. Because of the difference between the
+// spawned threads' sleep time, and the waiting threads sleep time, when you see 6 lines
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
