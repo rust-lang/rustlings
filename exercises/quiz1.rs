809271493c135763bcf513906dbe90955394d625
Vasu Sharma <vasu.sharma@blue-yonder.com>
2020-12-08 10:35:52 +0100

    Solutions: Part 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,16 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+
+fn calculate_apple_price(quantity: i32) -> i32 {
+    if quantity <= 40 {
+        quantity * 2
+    }
+    else {
+        quantity
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,7 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER: u32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
