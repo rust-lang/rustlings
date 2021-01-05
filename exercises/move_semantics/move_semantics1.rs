01f5455ea0e655935efc1874df858c9918e51373
Emre AYDIN <aeaydin1@gmail.com>
2021-01-05 15:33:43 +0300

    Move semantics and primitive_types exercises complete

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,10 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -17,7 +15,6 @@ fn main() {
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
-
     vec.push(22);
     vec.push(44);
     vec.push(66);
