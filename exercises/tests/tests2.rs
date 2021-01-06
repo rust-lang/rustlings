775744ed891a39ddacc8ddac3b64ef480cede48c
Emre AYDIN <aeaydin1@gmail.com>
2021-01-06 18:18:46 +0300

    Tests and quiz 3 exercises complete

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,10 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(2, 2);
     }
 }
