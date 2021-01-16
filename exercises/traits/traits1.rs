af9b7e1b0811ddfe05bb52459f2b9b24dfc74b60
David Bailey <davidbailey00@outlook.com>
2021-01-16 20:43:48 +0000

    Add traits solutions

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,14 +8,17 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+    fn append_bar(self) -> Self {
+        // self.push_str("Bar");
+        // self
+        // OR
+        format!("{}Bar", self)
+    }
 }
 
 fn main() {
