50c322d8c93a63bcf1a570fcb07dc6a1cf1bd07c
David Bailey <davidbailey00@outlook.com>
2021-01-16 20:43:05 +0000

    Add generics solutions

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -1,14 +1,12 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // Rewrite it using generics so that it supports wrapping ANY type.
 
-// I AM NOT DONE
-
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
