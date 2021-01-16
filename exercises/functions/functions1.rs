7d783c3a7b5ce2fe3c5ce07fc1d123c0f4db9b07
David Bailey <davidbailey00@outlook.com>
2021-01-16 20:36:50 +0000

    Add functions solutions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+}
+
+fn call_me() {
+    println!("Hello, world!");
 }
