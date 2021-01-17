e50c35eb7ccb6c12c906249c38235910d81421dc
David Bailey <davidbailey00@outlook.com>
2021-01-17 11:42:58 +0000

    Add clippy solutions

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,10 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (y - x).abs() > f64::EPSILON {
         println!("Success!");
     }
 }
