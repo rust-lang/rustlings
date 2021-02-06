495f2d29968c00a67a05de71ac659e74c7f65a09
Tal <3195851+tal-zvon@users.noreply.github.com>
2021-02-06 15:00:50 -0700

    Update move_semantics4.rs
    Removed redundant use of "instead"

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -1,6 +1,6 @@
 // move_semantics4.rs
 // Refactor this code so that instead of having `vec0` and creating the vector
-// in `fn main`, we instead create it within `fn fill_vec` and transfer the
+// in `fn main`, we create it within `fn fill_vec` and transfer the
 // freshly created vector from fill_vec to its caller.
 // Execute `rustlings hint move_semantics4` for hints!
 
