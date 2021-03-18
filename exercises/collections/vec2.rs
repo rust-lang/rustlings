bef39b125961310b34b34871e480a82e82af4678
Mickael Fortunato <morsi.morsicus@gmail.com>
2021-03-18 18:39:22 +0100

    fix(collections): Naming exercises for vectors and hashmap

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -28,11 +28,6 @@ mod tests {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_loop(v.clone());
 
-        assert_eq!(
-            ans,
-            v.iter()
-                .map(|x| x * 2)
-                .collect::<Vec<i32>>()
-        );
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
