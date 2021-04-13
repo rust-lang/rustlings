ec466877423afd3ea3627d17c0842d0849216c80
Simon Halimi <halimisimon@gmail.com>
2021-04-13 14:35:46 +0200

    first commit

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,13 +1,11 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(3);
 }
 
-fn call_me(num: u32) {
+fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
