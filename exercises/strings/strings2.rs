bf160e301c0adb942e51b9dd8a66fc02dc71c682
Simon Halimi <halimisimon@gmail.com>
2021-04-13 17:31:06 +0200

    quizz et string ajouté

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -1,12 +1,9 @@
 // strings2.rs
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` for hints :)
-
-// I AM NOT DONE
-
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
