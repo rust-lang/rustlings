261736a355d414b967ef0481f8e6c4e54ccaca8c
Pinklady01 <katyanne_mm@live.fr>
2021-04-13 15:32:53 +0200

    functions

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,6 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -9,5 +8,5 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    return num * num;
 }
