f37762c23a61d752561d6bd06730c598b5e184ef
Zachary Chua <zacchua@gmail.com>
2021-05-05 18:16:53 +0800

    Finished first 5 as wel as quiz1
    Finished variables, functions, if, move semantics, primitive types as
    well as quiz1

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,22 +2,19 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let mut vec0 = Vec::new();
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
+    let mut vec1 = fill_vec(&mut vec0);
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
