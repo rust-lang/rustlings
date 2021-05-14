10dabcd66c13354ec79570860318330bb027ee7a
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2021-05-13 21:40:20 -0500

    Complete option exercises (#15)

--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -1,12 +1,9 @@
 // option2.rs
 // Make me compile! Execute `rustlings hint option2` for hints
 
-// I AM NOT DONE
-
 fn main() {
     let optional_word = Some(String::from("rustlings"));
-    // TODO: Make this an if let statement whose value is "Some" type
-    word = optional_word {
+    if let Some(word) = optional_word {
         println!("The word is: {}", word);
     } else {
         println!("The optional word doesn't contain anything");
@@ -17,9 +14,7 @@ fn main() {
         optional_integers_vec.push(Some(x));
     }
 
-    // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
-    // You can stack `Option<T>`'s into while let and if let
-    integer = optional_integers_vec.pop() {
+    while let Some(Some(integer)) = optional_integers_vec.pop() {
         println!("current value: {}", integer);
     }
 }
