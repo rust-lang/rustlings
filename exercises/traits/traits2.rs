0ed1e16b9c10fb2b92572429bfd94343622cf265
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2021-05-13 21:40:49 -0500

    Complete trait exercises (#16)

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,16 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_string());
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
