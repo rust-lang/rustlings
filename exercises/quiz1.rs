5af7339508918ed281f80ec8df7a5108a6b37023
0xChasingFire <0xchasingfire@gmail.com>
2021-05-19 11:20:12 +0800

    variables if and functions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,16 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
+fn calculate_apple_price(apple: u32) -> u32{
+    if apple <= 40 {
+        apple * 2
+    } else {
+        apple * 1
+    }
+}
 
 // Don't modify this function!
 #[test]
