dd131cb487f78242661001e49787cec710415591
0xChasingFire <0xchasingfire@gmail.com>
2021-05-27 20:34:46 +0800

    threads and iter5

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -11,7 +11,6 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,6 +34,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    // map.keys().len()
+    count_for(map, value)
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -53,6 +54,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    count_collection_for(collection, value)
 }
 
 #[cfg(test)]
