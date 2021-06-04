1c2e74149541e28fdd552da69d3cab865bca9ae9
0xChasingFire <83760488+0xchasingfire@users.noreply.github.com>
2021-06-04 11:17:24 +0800

    macro and clippy

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -37,6 +37,7 @@ impl Default for Person {
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 { super::default()}
     }
 }
 
--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,12 +1,10 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
