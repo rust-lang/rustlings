a7163862a427c7c39de88ac0235d0e6c0f6bc043
YeSifan <yesifan66@gmail.com>
2021-06-19 20:44:07 +0800

    complete: functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    fn call_me(){
+        println!("call_me");
+    }
     call_me();
 }
