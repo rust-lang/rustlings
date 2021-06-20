c4a62515fb63d014f323cb67017514be1f0ad799
YeSifan <yesifan66@gmail.com>
2021-06-20 16:42:08 +0800

    complete: structs

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -1,8 +1,6 @@
 // structs2.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -12,6 +10,13 @@ struct Order {
     made_by_email: bool,
     item_number: u32,
     count: u32,
+}
+
+impl Order {
+    fn update<'a>(&mut self, value: &'a str){
+        self.count += 1;
+        self.name = String::from(value);
+    }
 }
 
 fn create_order_template() -> Order {
@@ -34,7 +39,8 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let mut your_order = create_order_template();
+        your_order.update("Hacker in Rust");
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
