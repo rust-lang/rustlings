4a08fff2b83c624a7bbc77c95131afba660eb3f9
seth <yesifan66@gmail.com>
2021-06-25 15:22:02 +0800

    complete: clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,11 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    let error_margin = f64::EPSILON;
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (y - x).abs() > error_margin { 
         println!("Success!");
     }
 }
