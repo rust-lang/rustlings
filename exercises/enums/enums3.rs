5480d357c37230f5b1a04144b1da7baf00078430
eribertt_fedorabox <erimendz@gmail.com>
2021-07-04 15:18:33 +0300

    add solutions to this exercise but incomplete

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,6 +5,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+	Move {x: i32, y: i32},
+	Echo (String),
+	ChangeColor (i32, i32, i32),
+	Quit,
 }
 
 struct Point {
@@ -37,6 +41,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+		match message {
+			Message::Move => {x: i32, y: i32},
+			Message::Echo => { String },
+			Message::ChangeColor => { i32, i32, i32 },
+			Message::Quit => {},
+			
+		}
     }
 }
 
@@ -51,9 +62,10 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
+        //state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Move { x: 10, y: 15 });
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
