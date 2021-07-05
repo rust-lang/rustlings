1bfa43a40f997d1b94a8d764970ecb53dd0d1fb9
Rakshit Sinha <rakshit.sinha@oracle.com>
2021-07-05 16:36:33 -0700

    chore: Updated quiz1.rs question description

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -5,7 +5,7 @@
 
 // Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
 // more than 40 at once, each apple only costs 1! Write a function that calculates
-// the price of an order of apples given the order amount. No hints this time!
+// the price of an order of apples given the quantity bought. No hints this time!
 
 // I AM NOT DONE
 
