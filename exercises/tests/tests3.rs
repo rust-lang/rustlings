b88fff0ab859bab5fdb813045509a75eba36eba0
BertrandBev <bertrand.bevillard@gmail.com>
2021-07-21 09:18:14 +0200

    fixed typo

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -1,5 +1,5 @@
 // tests3.rs
-// This test isn't testing our function -- make it do that in such a way that
+// This test isn't testing our function -- make it does that in such a way that
 // the test passes. Then write a second test that tests whether we get the result
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` for hints :)
