f8e766bc66aa00f12219feb8711f09b3f14b9664
Andrew Dobrych <andrewdo@nandos.co.uk>
2021-08-11 11:21:01 +0100

    exercises

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,14 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
+// Exercise explanation
+//you can declare a new variable with the same name as a previous variable.
+// Rustaceans say that the first variable is shadowed by the second, which means that the second variable’s value is what appears when the variable is used.
+// We can shadow a variable by using the same variable’s name and repeating the use of the let keyword
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
