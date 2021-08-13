b87a2323ae610475c8147e461b4361bf48da1a3b
Shmulik Cohen <anuk909@gmail.com>
2021-08-13 14:03:09 +0300

    add module3 exercise that shows uses of modules from std

---
+++ exercises/modules/modules3.rs
@@ -0,0 +1,17 @@
+// modules3.rs
+// You can use the 'use' keyword to bring module paths from modules from anywere
+// and espically from the rust standard library. Bring SystemTime and UNIX_EPOCH
+// from the std::time module. Bonus style points if you can do it with one line!
+// Make me compile! Execute `rustlings hint modules3` for hints :)
+
+// I AM NOT DONE
+
+// TODO: Complete this use statement
+use ???
+
+fn main() {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
