e730260ad4a673bfbdbd14d67cbd42a89b7ff306
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-09-14 10:19:14 -0300

    Solve enum exercises.

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,12 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Quit,
+    Echo(String),
+    Move {x: i32, y: i32},
+    ChangeColor(i32, i32, i32),
 }
 
 impl Message {
