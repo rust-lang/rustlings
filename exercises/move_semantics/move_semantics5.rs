f2e63bb0c0b3443f661066ef73ebb6a552c87698
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-09-14 10:17:39 -0300

    Solve move semantics exercises.

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -1,15 +1,13 @@
 // move_semantics5.rs
-// Make me compile only by reordering the lines in `main()`, but without
-// adding, changing or removing any of them.
+// Make me compile without adding, removing, or changing any of the
+// lines in `main()`.
 // Execute `rustlings hint move_semantics5` for hints :)
-
-// I AM NOT DONE
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
-    *y += 100;
+    let z = &mut *y;
     *z += 1000;
+    *y += 100;
     assert_eq!(x, 1200);
 }
