d9b1cfd1a3ff313fddcfd20ff27b17cac7dfd758
Jack Harper <jack.harper@stfc.ac.uk>
2021-09-25 11:22:21 +0100

    finished structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,8 +1,6 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 struct ColorClassicStruct {
     name: String,
     hex: String
@@ -17,14 +15,10 @@ struct UnitStruct;
 mod tests {
     use super::*;
 
-    static name:String = String::from("green");
-    static hex:String = String::from("#00FF00");
-
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        let green = ColorClassicStruct(name:name, hex:hex);
-
+        let green = ColorClassicStruct { name: String::from("green"), hex: String::from("#00FF00")};
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
     }
@@ -32,7 +26,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct(name, hex);
+        let green = ColorTupleStruct(String::from("green"), String::from("#00FF00"));
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -41,7 +35,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        let unit_struct = UnitStruct(name, hex);
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
