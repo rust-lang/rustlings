e282e897d2b0e5223ba67374de78e9cb85753e11
blacktoast <dbstjd100@naver.com>
2021-09-28 05:44:39 +0000

    test2

--- exercises/variables/variables4.rs
+++
@@ -1,9 +0,0 @@
-// variables4.rs
-// Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
-
-// I AM NOT DONE
-
-fn main() {
-    let x: i32;
-    println!("Number {}", x);
-}
--- exercises/variables/variables6.rs
+++
@@ -1,9 +0,0 @@
-// variables6.rs
-// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
-
-// I AM NOT DONE
-
-const NUMBER = 3;
-fn main() {
-    println!("Number {}", NUMBER);
-}
