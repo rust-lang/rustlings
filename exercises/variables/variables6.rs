30b4480ca3a1ac28e084069e5d2013606a552768
blacktoast <dbstjd100@naver.com>
2021-09-28 05:54:31 +0000

    test

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,11 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
+/*
+I AM NOT DONE
+*/
+const NUMBER=1;
 
-const NUMBER = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
