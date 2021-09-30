2a40efb29b33c0a6de8482a5a3d6c4e97a630e16
blacktoast <dbstjd100@naver.com>
2021-09-30 07:41:34 +0000

    primitive ing,,

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,7 +5,7 @@
 // I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = String::from("Test hihihhihihihihihihihihihihiihihihihi");
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
