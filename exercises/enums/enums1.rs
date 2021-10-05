5a518df12139a1406b2057d50ced6ca057e6c7a0
blacktoast <dbstjd100@naver.com>
2021-10-05 21:12:24 +0900

    t

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -5,10 +5,15 @@
 
 #[derive(Debug)]
 enum Message {
+    Quit,
+    Echo,
+    Move,
+    ChangeColor
     // TODO: define a few types of messages as used below
 }
 
 fn main() {
+    
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
