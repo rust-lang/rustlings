14d63e2b8e2eaf9c1610a6b5d66d0cfcceb49e7a
blacktoast <dbstjd100@naver.com>
2021-10-12 08:13:51 +0000

    vec done

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,10 +7,13 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
+        println!("{}",i);
+        *i=*i*2;
+        println!("{}",i);
+
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
     }
@@ -26,8 +29,8 @@ mod tests {
     #[test]
     fn test_vec_loop() {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let v2:Vec<i32>=(1..13).collect();
         let ans = vec_loop(v.clone());
-
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
