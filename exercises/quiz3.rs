f57cd506476ec66ca1c1795123b4d8cc3a8b5e48
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-10-13 17:28:54 -0300

    Solve quiz3.

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -7,7 +7,6 @@
 // we expect to get when we call `times_two` with a negative number.
 // No hints, you can do this :)
 
-// I AM NOT DONE
 
 pub fn times_two(num: i32) -> i32 {
     num * 2
@@ -19,12 +18,11 @@ mod tests {
 
     #[test]
     fn returns_twice_of_positive_numbers() {
-        assert_eq!(times_two(4), ???);
+        assert_eq!(times_two(4), 8);
     }
 
     #[test]
     fn returns_twice_of_negative_numbers() {
-        // TODO replace unimplemented!() with an assert for `times_two(-4)`
-        unimplemented!()
+        assert_eq!(times_two(-4), -8);
     }
 }
