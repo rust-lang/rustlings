bacb5fac1857e506fde502d756549d9741cb97c3
Sky Yin <sky.yin@gmail.com>
2021-10-16 18:03:09 -0700

    Done exercises on variables

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,9 +1,8 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    let x: i32;
+    let x: i32 = 10;
     println!("Number {}", x);
 }
