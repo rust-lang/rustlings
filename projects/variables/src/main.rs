7edf4a788800c4a7029fed7d7dc9f6f9c78c8636
tobyatgithub <toby.fangyuan@gmail.com>
2021-10-22 16:22:09 -0700

    cache chapter 3.0

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    const MAX_POINTS: u32 = 100_000;
+    // let x = 5;
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
