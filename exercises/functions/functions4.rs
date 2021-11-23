2e8abe5a8f1ba20c22e192585e72a8d5ac4222eb
Abhijit Roy <dalvath3700@gmail.com>
2021-11-23 12:27:17 +0530

    functions4 modified

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -11,7 +11,7 @@ fn main() {
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: u32) -> u32 {
     if is_even(price) {
         price - 10
     } else {
@@ -19,6 +19,6 @@ fn sale_price(price: i32) -> {
     }
 }
 
-fn is_even(num: i32) -> bool {
+fn is_even(num: u32) -> bool {
     num % 2 == 0
 }
