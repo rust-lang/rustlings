248972ed0425d76a8693f85c97df21d7a82bf307
tobyatgithub <toby.fangyuan@gmail.com>
2021-12-29 17:34:32 -0800

    fix the loop bug, made game work.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -4,12 +4,11 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    // println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
-
-        let secret_number = rand::thread_rng().gen_range(1..101);
-        println!("The secret number is: {}", secret_number);
         let mut guess = String::new();
 
         io::stdin()
