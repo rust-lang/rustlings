ace380fd5dd25931363e25e15a82b86bff75c937
camperdue42 <camperdue42@gmail.com>
2021-12-31 01:05:34 -0500

    fix(structs3): Add panic! statement into structs3
    closes #685

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -15,7 +15,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Can not ship a weightless package.")
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
