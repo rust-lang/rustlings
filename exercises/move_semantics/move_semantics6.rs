b5205759f09986143ae2c51f26ac69c4929a50fd
Kallu-A <lucas.aries@protonmail.com>
2022-01-08 01:28:00 +0100

    feat: Add move_semantics6.rs exercise

---
+++ exercises/move_semantics/move_semantics6.rs
@@ -0,0 +1,24 @@
+// move_semantics6.rs
+// Make me compile! `rustlings hint move_semantics6` for hints
+// You can't change anything except adding or removing reference
+
+// I AM NOT DONE
+
+fn main() {
+    let data = "Rust is great!".to_string();
+    println!("{}", get_char(data));
+
+
+    string_uppercase(&data);
+}
+
+// Should not take ownership
+fn get_char(data: String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: &String) {
+    data = &data.to_uppercase();
+    println!("{}", data);
+}
