c1655da38f918a224650f35aeae53086ea941f6b
mustakimur khandaker <mrkhandaker@uga.edu>
2022-02-08 10:39:59 -0500

    simpler exercise for now

---
+++ exercises/students/README.md
@@ -0,0 +1,6 @@
+In this assignment, you will practice writing code relating to struct and ownership. Follow the code comments.
+- Ensure you are not missing any shorthands possible we have discussed.
+- Define methods means you call that using a `instance.fn()` approach.
+- You can have regular functions to iterate over the vector and sorting (ascending or descending) reason.
+- Make sure you follow best practices.
+  - Minimize number of warnings, if possible to zero.
---
+++ exercises/students/students.rs
@@ -0,0 +1,38 @@
+struct Student {
+   name: String,
+   id: i32,
+   major: String,
+   gradyear: i32,
+}
+
+// define associate functions to create the following instances, use shorthand wherever available
+
+// define methods to satisfy the requirements
+
+// define functions to satisfy the requirements
+
+fn main() {
+   // a vector<Student> and push the following instances
+
+   // an instance of student (Adam Helex, 1022, CS, 2022)
+
+   // an instance of student (Rob Delon, 1033, Bio, 2023)
+
+   // an instance of student (Jenny Susan, 1044, CS, 2023)
+
+   // an instance of student (Nick Frank, 1055, CS, 2022)
+
+   // an instance of student (Michael Dodge, 1066, Bio, 2023)
+
+   // Note: shorthand possible for Rob and Michael; Adam and Helex
+
+   // call a function that will return list of students graduating by year
+
+   // call a function that will return list of the last name of students
+
+   // call a function that will sort the students by their first name
+
+   // call a method that will return true if any two students are graduating same year
+
+   // call a method that will modify the student last name to a desired one
+}
