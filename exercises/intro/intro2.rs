d6fbbecaa595d86f1e13aa55a8018fac40c3598c
Justin Kelz <justinkelz@gmail.com>
2022-03-12 10:11:38 -0800

    Completed the intro

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,8 +2,9 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!","world");
+    // you need to put in a string with double quotes. Not sure what about 'world' makes it break yet.
+    // It seems that a string literal needs double quotes.
 }
