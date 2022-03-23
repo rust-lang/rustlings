7e2bfca1aff45f56b39abe4bc09dc079bdce3b2e
Justin Kelz <justinkelz@gmail.com>
2022-03-23 15:01:28 -0700

    Through Quiz 1

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,6 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -9,5 +8,7 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+     num * num // This is really subtle, here we're not, stricly speaking, returning anything.
+                // We're simply writing an expression (because there's no ; if there was it'd be a   
+                // "statement")
 }
--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,9 +2,8 @@
 // Make me compile!
 // Execute the command `rustlings hint variables1` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
