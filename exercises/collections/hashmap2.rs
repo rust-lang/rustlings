754a006da822873c1864a08b214bd043bf73ce0c
Justin Kelz <justinkelz@gmail.com>
2022-03-28 21:07:12 -0700

    Got all the way to errors!

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -13,7 +13,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,10 +34,20 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
-    for fruit in fruit_kinds {
+    for fruit in fruit_kinds { // This is sujestive of vectors being iterable
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit).or_insert(1);
+        /*
+            So, this is just saying "Hey, add 1
+            of this fruit if it hasn't already 
+            been added."
+        */
+        
+
+
+
     }
 }
 
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,17 +2,16 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&*word) { // So I needed to match types here 
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
+fn is_a_color_word(attempt: &str) -> bool { // The type specified here needed to be respected when we passed the variable word into the function/
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
