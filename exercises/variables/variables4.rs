33920c9143bff410aaaf4bde5c6584069ae790bf
ynhhoJ <lJohhnyl@outlook.com>
2022-04-16 11:40:20 +0300

    🚧Variables: Exercise 4

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,9 +1,9 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let x: i32;
+    let x: i32 = 1337;
     println!("Number {}", x);
 }
