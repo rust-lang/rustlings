05148686069ee7740fb92ac1df729418c5d0b2fe
ynhhoJ <lJohhnyl@outlook.com>
2022-04-16 11:37:10 +0300

    🚧Variables: Exercise 2

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,10 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let x;
+    let x = 10;
     if x == 10 {
         println!("Ten!");
     } else {
