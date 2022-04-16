a3d18c416b540cb75271f458b4641eafb7ee1c5b
ynhhoJ <lJohhnyl@outlook.com>
2022-04-16 11:38:12 +0300

    🚧Variables: Exercise 3

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,10 +1,10 @@
 // variables3.rs
 // Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let x = 3;
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
