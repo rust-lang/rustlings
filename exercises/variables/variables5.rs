dd9e17d236cea9414e2d577638f18647e7599543
ynhhoJ <lJohhnyl@outlook.com>
2022-04-16 11:52:42 +0300

    🚧Variables: Exercise 5

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,13 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
-    println!("Number plus two is : {}", number + 2);
+    {
+        let number = 3;
+        println!("Number plus two is : {}", number + 2);
+    }
 }
