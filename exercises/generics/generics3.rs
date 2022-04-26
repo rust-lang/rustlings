a728d1d495baae9eeee557f9a30b1c491cacf420
ludop31 <ludovic.peyronnet@gmail.com>
2022-04-26 23:22:40 +0200

    Correction of the seccond test for excercice generics3.rs there was an error in the function generate_alphabetic_report_card(), 2.1 instead of A+

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -46,7 +46,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
