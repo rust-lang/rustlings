de77d1b223399c828793959d2d24cd7ac03cffe8
Justin Kelz <justinkelz@gmail.com>
2022-05-01 08:33:02 -0700

    Added comments to generics2

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -3,13 +3,13 @@
 
 // Execute `rustlings hint generics2` for hints!
 
-// I AM NOT DONE
+// Looking at the diff of this file and my changes should give enough exposition.
 
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> { // We modify this to accept any type T, and then we'll implement it down below
+    value: T,
 }
 
-impl Wrapper {
+impl<u32> Wrapper<u32> { // so here you'll notice that I provide impl with a type, as I also specify a type for the wrapper, this allows us to handle the u32 case.
     pub fn new(value: u32) -> Self {
         Wrapper { value }
     }
