424140356a64ad8f330829ceae1ab218d7e9b6e0
conglt-0917 <conglt1712@gmail.com>
2022-05-12 18:00:12 +0700

    answers excersise

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,13 +1,16 @@
 // if1.rs
 
-// I AM NOT DONE
-
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
     // - additional variables
     // Execute `rustlings hint if1` for hints
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 // Don't mind this for now :)
