dc6de2e9b3355fe9d84b2db7eb7bba735ee5518f
Chris Girvin <cgm82589933@outlook.com>
2022-05-18 21:33:45 -0400

    quiz2 done

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -38,6 +36,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Apple => basket.entry(fruit).or_insert(4),
+            Fruit::Mango => basket.entry(fruit).or_insert(2),
+            Fruit::Lychee => basket.entry(fruit).or_insert(5),
+            Fruit::Pineapple => basket.entry(fruit).or_insert(5),
+            Fruit::Banana => basket.entry(fruit).or_insert(5),
+        };
     }
 }
 
--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -1,8 +1,6 @@
 // strings1.rs
 // Make me compile without changing the function signature!
-// Execute `rustlings hint strings1` for hints ;)
-
-// I AM NOT DONE
+// Execute `rustlings hint strings1` for hints ;
 
 fn main() {
     let answer = current_favorite_color();
@@ -10,5 +8,5 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
