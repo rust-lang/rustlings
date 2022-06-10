ed18c3b93dbc065334ded943611e7367996a1da9
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-06-10 17:39:11 +0200

    added(save): random save

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,8 +1,12 @@
 // if1.rs
 
-// I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a
+    } else {
+        return b;
+    }
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
