558dacfa2b09aba8cf0617dead6f494a903e6529
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-06-16 23:47:50 +0200

    added(test): save

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,7 +8,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 fn calculate_apple_price(quantity: i32) -> i32 {
