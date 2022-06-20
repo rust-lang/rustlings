455cae6f2875f160508162525b0283de0098ef39
Stanislav Pankrashin <stanislav.pankrashin@roamdigital.com>
2022-06-21 09:30:00 +1200

    finished first batch

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,8 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    let x: u8 = 10;
     if x == 10 {
         println!("Ten!");
     } else {
