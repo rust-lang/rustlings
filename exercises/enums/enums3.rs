9e3329d2779985e1dd046e6d852f5f1ad3b8de13
Stanislav Pankrashin <stanislav.pankrashin@roamdigital.com>
2022-06-21 15:40:42 +1200

    did some more

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -36,7 +37,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Move(point) => self.move_position(point),
+            Message::ChangeColor((r, g ,b)) => self.change_color((r, g ,b)),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
