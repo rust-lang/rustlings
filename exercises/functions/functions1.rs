e83883dead5b76ce704b292256c2559bca670564
Stanislav Pankrashin <stanislav.pankrashin@roamdigital.com>
2022-06-21 13:34:09 +1200

    finished more

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+fn call_me() -> i32 {
+    1
+}
 
 fn main() {
     call_me();
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,12 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(number_of_apples: u32) -> u32 {
+    let price = if number_of_apples <= 40 {2} else {1};
+
+    number_of_apples * price
+}
 
 // Don't modify this function!
 #[test]
