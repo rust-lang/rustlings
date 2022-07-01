761945c40c10f4f72efe3e9193f8b9896ff40f27
CelestialMelody <celetialmelodygo@gmail.com>
2022-07-01 17:42:18 +0800

    day one

---
+++ I_have_done/intro/intro2.rs
@@ -0,0 +1,7 @@
+// intro2.rs
+// Make the code print a greeting to the world.
+// Execute `rustlings hint intro2` for a hint.
+
+fn main() {
+    println!("Hello!");
+}
---
+++ I_have_done/variables/mod.rs
@@ -0,0 +1,6 @@
+mod variables1;
+mod variables2;
+mod variables3;
+mod variables4;
+mod variables5;
+mod variables6;
