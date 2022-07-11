c3c21ad91fd66bcffedb85f542c1c830e1a7cdc0
mokou <mokou@fastmail.com>
2022-07-11 13:35:16 +0200

    fix(intro): clarify hint usage

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -3,7 +3,7 @@
 // We sometimes encourage you to keep trying things on a given exercise, even
 // after you already figured it out. If you got everything working and feel
 // ready for the next exercise, remove the `I AM NOT DONE` comment below.
-// Execute the command `rustlings hint intro1` for a hint.
+// Execute `rustlings hint intro1` or use the `hint` watch subcommand for a hint.
 //
 // If you're running this using `rustlings watch`: The exercise file will be reloaded
 // when you change one of the lines below! Try adding a `println!` line, or try changing
--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,6 +1,6 @@
 // intro2.rs
 // Make the code print a greeting to the world.
-// Execute `rustlings hint intro2` for a hint.
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
 
