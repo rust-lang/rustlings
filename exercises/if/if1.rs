4868d18ea315f15ad007086f218c8a1ee68a8eb4
mokou <mokou@fastmail.com>
2022-07-12 11:10:08 +0200

    feat(if): replace hints

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,4 +1,5 @@
 // if1.rs
+// Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
 
@@ -7,7 +8,6 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
-    // Execute `rustlings hint if1` for hints
 }
 
 // Don't mind this for now :)
