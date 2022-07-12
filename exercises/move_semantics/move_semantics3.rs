2e62505143a726c610a60cc8f11fbb4b0449ba8f
mokou <mokou@fastmail.com>
2022-07-12 15:43:26 +0200

    feat(move_semantics): finish updating comments

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -1,7 +1,7 @@
 // move_semantics3.rs
 // Make me compile without adding new lines-- just changing existing lines!
 // (no lines with multiple semicolons necessary!)
-// Execute `rustlings hint move_semantics3` for hints :)
+// Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
 
