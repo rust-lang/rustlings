016d718a286f6ccacaecdaa81d9245e96424bae1
mokou <mokou@fastmail.com>
2022-07-14 18:15:47 +0200

    feat(tests): add hint comments

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -1,6 +1,7 @@
 // tests2.rs
 // This test has a problem with it -- make the test compile! Make the test
-// pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
+// pass! Make the test fail!
+// Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
 
