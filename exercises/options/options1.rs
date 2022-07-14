06e4fd376586709082664a304f0394244d4ab6bd
mokou <mokou@fastmail.com>
2022-07-14 17:53:27 +0200

    feat(options1): rewrite to remove array stuff

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -8,16 +8,30 @@ fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
 }
 
-fn main() {
-    print_number(13);
-    print_number(99);
+// This function returns how much icecream there is left in the fridge.
+// If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
+// all, so there'll be no more left :(
+// TODO: Return an Option!
+fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+    // We use the 24-hour system here, so 10PM is a value of 22
+    ???
+}
 
-    let mut numbers: [Option<u16>; 5];
-    for iter in 0..5 {
-        let number_to_add: u16 = {
-            ((iter * 1235) + 2) / (4 * 16)
-        };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        numbers[iter as usize] = number_to_add;
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(10), Some(5));
+        assert_eq!(maybe_icecream(23), None);
+        assert_eq!(maybe_icecream(22), None);
+    }
+
+    #[test]
+    fn raw_value() {
+        // TODO: Fix this test. How do you get at the value contained in the Option?
+        let icecreams = maybe_icecream(12);
+        assert_eq!(icecreams, 5);
     }
 }
