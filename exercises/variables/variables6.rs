9b4e1a0b93b1c0f41092a9419440940788149761
darko.deuric <darko.deuric@ethernal.tech>
2022-08-05 12:11:06 +0200

    Intro and Variables

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,8 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+const NUMBER: u32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
