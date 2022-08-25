2177e3e1f940433eec4adaa70bd4a4002366c336
locriacyber <74560659+locriacyber@users.noreply.github.com>
2022-08-25 22:23:06 +0000

    Fix quiz1
    I can't believe this... **40 or more**

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -17,10 +17,12 @@
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
-    let price2 = calculate_price_of_apples(40);
-    let price3 = calculate_price_of_apples(65);
+    let price2 = calculate_price_of_apples(39);
+    let price3 = calculate_price_of_apples(40);
+    let price4 = calculate_price_of_apples(65);
 
     assert_eq!(70, price1);
-    assert_eq!(80, price2);
-    assert_eq!(65, price3);
+    assert_eq!(78, price2);
+    assert_eq!(40, price3);
+    assert_eq!(65, price4);
 }
