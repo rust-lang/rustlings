e05ae03eb9c5aed7b026ab4365511b1a716665cb
ceth <mail@demas.cc>
2022-08-31 15:53:41 +0300

    fix

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -5,7 +5,7 @@
 // - If
 
 // Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
-// 40 or more at once, each apple only costs 1! Write a function that calculates
+// more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
 // I AM NOT DONE
@@ -21,6 +21,6 @@ fn verify_test() {
     let price3 = calculate_price_of_apples(65);
 
     assert_eq!(70, price1);
-    assert_eq!(40, price2);
+    assert_eq!(80, price2);
     assert_eq!(65, price3);
 }
