9e720cf0ff7c647be578e4bf6d88a26b2ef8bd02
bahamoth <bahamoth@naver.com>
2022-09-03 21:51:17 +0900

    exercise done: structs1

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -15,7 +15,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
