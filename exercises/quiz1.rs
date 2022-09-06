1d5700e58a6f55a5549dc217aa9c4d071cdf8d2e
liv <mokou@fastmail.com>
2022-09-06 12:10:53 +0200

    fix(quiz1): add fourth assert

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -20,9 +20,11 @@
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
-    let price3 = calculate_price_of_apples(65);
+    let price3 = calculate_price_of_apples(41);
+    let price4 = calculate_price_of_apples(65);
 
     assert_eq!(70, price1);
     assert_eq!(80, price2);
-    assert_eq!(65, price3);
+    assert_eq!(41, price3);
+    assert_eq!(65, price4);
 }
