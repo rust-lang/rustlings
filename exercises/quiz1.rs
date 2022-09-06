96552e07c20ef9ad6b2d64d6f853cb8ff1da1b01
liv <mokou@fastmail.com>
2022-09-06 12:09:43 +0200

    fix(quiz1): correct explanation once again

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -4,9 +4,11 @@
 // - Functions
 // - If
 
-// Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
-// 40 or more at once, each apple only costs 1! Write a function that calculates
-// the price of an order of apples given the quantity bought. No hints this time!
+// Mary is buying apples. The price of an apple is calculated as follows:
+// - An apple costs 2 rustbucks.
+// - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
+// Write a function that calculates the price of an order of apples given
+// the quantity bought. No hints this time!
 
 // I AM NOT DONE
 
