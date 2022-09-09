8d9a1461d31a54be1fac0eee2f0443f1cb5a75ed
jojimanos <manosgeorge7@gmail.com>
2022-09-09 23:40:59 +0300

    Initial Commit

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,13 +1,11 @@
 // functions3.rs
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(0);
 }
 
-fn call_me(num: u32) {
+fn call_me(num: u32){
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
