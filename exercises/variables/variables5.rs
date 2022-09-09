b7bcbed9fa6f823de04acaf2fd66e32457d72c45
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-09-09 14:33:14 +0300

    shadowing

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -6,6 +6,8 @@
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    {
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
 }
