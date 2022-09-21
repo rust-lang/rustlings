8781a284585383bf9eca964aa6d3c4904efd8d5e
Daniil Moiseev <ksusonic@ya.ru>
2022-09-21 21:11:17 +0000

    privitive_types

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint primitive_types2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     // Characters (`char`)
 
@@ -22,7 +20,7 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
+    let your_character = 'A'; // Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
