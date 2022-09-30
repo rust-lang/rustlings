d6a603d8ce759071b95dd3948ba35de86531fb9a
Ruud Salym Erie <ruud@oply.co>
2022-09-30 11:51:20 -0300

    excercises so far

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -7,14 +7,13 @@
 // in the signatures for now. If anything, this is a good way to peek ahead
 // to future exercises!)
 
-// I AM NOT DONE
 
 fn main() {
-    let original_price = 51;
+    let original_price = 50;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> i32{
     if is_even(price) {
         price - 10
     } else {
@@ -23,5 +22,9 @@ fn sale_price(price: i32) -> {
 }
 
 fn is_even(num: i32) -> bool {
-    num % 2 == 0
+    let mut iseven =  false;
+   if num % 2 == 0{
+    iseven = true;
+   }
+   return iseven;
 }
