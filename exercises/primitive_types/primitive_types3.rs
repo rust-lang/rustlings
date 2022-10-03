9dc826f48a08ff68d3a8c2f74232d9d0cd0ae996
Ruud Salym J Erie <ruud@oply.co>
2022-10-03 15:54:17 -0300

    finished primitives types, quiz and vecs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,12 +2,12 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = 100;
+    let zero_vec = vec![3; a];
 
-    if a.len() >= 100 {
+    if zero_vec.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (total_apples:i32) -> i32 {
+    let mut cost = 2;
+    if total_apples > 40{
+        cost = 1;
+    }
+    return total_apples * cost;
+}
 
 // Don't modify this function!
 #[test]
