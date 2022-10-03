50adf98d5a72dc175710660780e7c8725d6ecfca
Alex Sudbinin <alex.txt@gmail.com>
2022-10-02 22:13:45 -0700

    hashmaps, modules, and quiz #2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -35,11 +33,36 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        let team_1_updated_status = match scores.get(&team_1_name) {
+            Some(team) => Team {
+                name: team_1_name,
+                goals_scored: team.goals_scored + team_1_score,
+                goals_conceded: team.goals_conceded + team_2_score,
+            },
+            None => Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            },
+        };
+
+        scores.insert(team_1_updated_status.name.clone(), team_1_updated_status);
+
+        let team_2_updated_status = match scores.get(&team_2_name) {
+            Some(team) => Team {
+                name: team_2_name,
+                goals_scored: team.goals_scored + team_2_score,
+                goals_conceded: team.goals_conceded + team_1_score,
+            },
+            None => Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            },
+        };
+
+        scores.insert(team_2_updated_status.name.clone(), team_2_updated_status);
     }
     scores
 }
