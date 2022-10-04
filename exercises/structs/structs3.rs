76392d81fa2689d22ccf8dc0cf774e8d345cb80d
Duchoud Nicolas <34117620+duchonic@users.noreply.github.com>
2022-10-04 11:43:23 +0200

    Added spaces around *

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -78,6 +78,6 @@ mod tests {
         let package = Package::new(sender_country, recipient_country, 1500);
 
         assert_eq!(package.get_fees(cents_per_gram), 4500);
-        assert_eq!(package.get_fees(cents_per_gram*2), 9000);
+        assert_eq!(package.get_fees(cents_per_gram * 2), 9000);
     }
 }
