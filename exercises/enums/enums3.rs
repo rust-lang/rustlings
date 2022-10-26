cb7cb46f567174ab8b75686ae2851942308281d0
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2022-10-26 17:47:43 +0530

    Updating solutions

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Quit,
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8,u8,u8))
 }
 
 struct Point {
@@ -38,6 +40,20 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(color) => {
+                self.change_color(color)                
+            }
+            Message::Echo(string) => {
+                self.echo(string)
+            }
+            Message::Quit => {
+                self.quit()
+            }
+            Message::Move(point) => {
+                self.move_position(point)
+            }
+        }
     }
 }
 
--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -2,7 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Order {
@@ -17,13 +16,13 @@ struct Order {
 
 fn create_order_template() -> Order {
     Order {
-        name: String::from("Bob"),
+        name: String::from("Hacker in Rust"),
         year: 2019,
         made_by_phone: false,
         made_by_mobile: false,
         made_by_email: true,
         item_number: 123,
-        count: 0,
+        count: 1,
     }
 }
 
@@ -35,7 +34,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = create_order_template();
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
