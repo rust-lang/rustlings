54b3be8264e674e3b4b201ef9298494b6d98ec96
liruifengv <liruifeng1024@gmail.com>
2022-11-03 10:58:46 +0800

    quiz

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+  let mut price = 0;
+  if num <= 40 {
+    price = num * 2;
+  } else {
+    price =  num * 1;
+  }
+  price
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -19,7 +19,6 @@
 // No hints this time!
 
 // I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,12 +28,25 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                  let str = String::from(string).to_uppercase();
+                  output.push(str)
+                },
+                Command::Trim => {
+                  let str = String::from(string).trim().into();
+                  output.push(str)
+                },
+                Command::Append(_n) => {
+                  let mut str = String::from(string);
+                  let a = "bar".repeat(*_n);
+                  str.push_str(a.as_str());
+                  output.push(str)
+                },
+            }
         }
         output
     }
@@ -42,8 +54,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
