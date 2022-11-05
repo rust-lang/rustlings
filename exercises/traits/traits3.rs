08d508556c0cdcd35683dde2b415d22ea83ca463
young <young@contxts.io>
2022-11-05 18:08:14 +0900

    traits3

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -10,7 +10,9 @@
 // I AM NOT DONE
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
