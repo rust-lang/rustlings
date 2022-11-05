1d646b414bf9a82d2bc062335fb7d426207af3ed
young <young@contxts.io>
2022-11-05 23:16:30 +0900

    traits4

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -20,7 +20,7 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types(software: impl Licensed, software_two: impl Licensed) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
