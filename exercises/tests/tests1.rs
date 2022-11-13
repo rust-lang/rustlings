d27e3281f8b861093b49e71a63823d9ad053a3e1
enforcer007 <akhilgodvsdemon@gmail.com>
2022-11-13 14:34:24 +0530

    tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -7,12 +7,10 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
     }
 }
