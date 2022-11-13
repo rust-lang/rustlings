6284d03eb10c66735a37d410cbd376d0c252a6ed
enforcer007 <akhilgodvsdemon@gmail.com>
2022-11-13 16:53:55 +0530

    macros

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -1,8 +1,6 @@
 // macros1.rs
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
@@ -10,5 +8,5 @@ macro_rules! my_macro {
 }
 
 fn main() {
-    my_macro();
+    my_macro!();
 }
