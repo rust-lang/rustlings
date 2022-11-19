ee8cf6a26b1848cfc7660ae8e6dd7cd2c369ee8f
Max Boewer <max.boewer@radical.onl>
2022-11-19 19:27:01 +0100

    Completed all tasks related to primitive types

--- exercises/primitive_types/primitive_types1.rs
+++ exercises/primitive_types/primitive_types1.rs
@@ -2,18 +2,17 @@
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
 
-// I AM NOT DONE
 
 fn main() {
     // Booleans (`bool`)
 
     let is_morning = true;
-    if is_morning {
+    if  is_morning == true {
         println!("Good morning!");
     }
 
-    let // Finish the rest of this line like the example! Or make it be false!
-    if is_evening {
+    let is_evening = true;// Finish the rest of this line like the example! Or make it be false!
+    if is_evening == true{
         println!("Good evening!");
     }
 }
