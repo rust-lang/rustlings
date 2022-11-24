db53dbc12615888ddd021025379fbab8e00e5067
TK Buristrakul <tkburis@gmail.com>
2022-11-24 19:41:25 +0000

    chore: tidied up unmatched backticks

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -2,7 +2,7 @@
 // Time to implement some traits!
 //
 // Your task is to implement the trait
-// `AppendBar' for the type `String'.
+// `AppendBar` for the type `String`.
 //
 // The trait AppendBar has only one function,
 // which appends "Bar" to any object
