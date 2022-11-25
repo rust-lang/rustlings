412eec553220f5b622b2d58837e9ac7a045f8450
Vikinguard <115805628+Vikinguard@users.noreply.github.com>
2022-11-25 01:03:21 +0100

    Update hashmaps2.rs

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -34,7 +34,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
+        fruit_basket.entry(String::from("Banana")).or_insert(10);// TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
     }
