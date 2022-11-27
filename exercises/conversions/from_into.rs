c2b9b200d23d733bcbe63ea3193ca4be59f8e064
MatyTan <1396568121@qq.com>
2022-11-27 10:38:11 +0800

    day12

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -2,6 +2,7 @@
 // If From is implemented correctly for a type, the Into trait should work conversely.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.From.html
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a hint.
+use std::num::ParseIntError;
 
 #[derive(Debug)]
 struct Person {
@@ -27,18 +28,50 @@ impl Default for Person {
 // be handled appropriately.
 //
 // Steps:
-// 1. If the length of the provided string is 0, then return the default of Person
-// 2. Split the given string on the commas present in it
-// 3. Extract the first element from the split operation and use it as the name
+// 1. If the length of the provided string is 0, then return the default of Person 如果为0
+//    返回defalut
+// 2. Split the given string on the commas present in it 逗号分割
+// 3. Extract the first element from the split operation and use it as the name 第一个元素
 // 4. If the name is empty, then return the default of Person
 // 5. Extract the other element from the split operation and parse it into a `usize` as the age
+//    提取年龄
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let default = Person::default();
+        let mut p = Person {
+            name: String::from(""),
+            age: 0,
+        };
+        if s.len() == 0 {
+            return default;
+        }
+
+        let person: Vec<&str> = s.split(",").collect();
+        if person.len() != 2 {
+            return default;
+        }
+
+        let name = person[0];
+
+        if name.len() <= 0 {
+            return default;
+        }
+        p.name = name.to_string();
+
+        let age = person[1];
+        let res: Result<usize, ParseIntError> = age.parse();
+        match res {
+            Ok(a) => p.age = a,
+            Err(err) => {
+                println!("{:?}", err);
+                return default;
+            }
+        };
+
+        p
     }
 }
 
