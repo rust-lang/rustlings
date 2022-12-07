48aa9287263181de23607d24ebe52c237243cc76
Max Boewer <max.boewer@radical.onl>
2022-12-07 23:59:40 +0100

    Finished enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,13 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -37,7 +40,20 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message{
+            Message::ChangeColor(color) =>{
+                self.change_color(color);
+            }
+            Message::Echo(string) =>{
+                self.echo(string);
+            }
+            Message::Move(point) =>{
+                self.move_position(point);
+            }
+            Message::Quit =>{
+                self.quit();
+            }
+        }
     }
 }
 
