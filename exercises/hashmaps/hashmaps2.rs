0971fb6dec48867b6753148c86b2158f3171ce3c
Ruud Salym J Erie <ruud@oply.co>
2022-12-07 20:48:38 -0300

    finished hasmap excercises and

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,6 +35,9 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -24,6 +22,22 @@ struct Team {
     goals_scored: u8,
     goals_conceded: u8,
 }
+impl Team {
+    fn named(name: String) -> Self {
+         Self { name: name.to_string(), goals_scored: 0, goals_conceded: 0}
+        }
+}
+fn update_score(
+    scores: &mut HashMap<String, Team>, 
+    name: String, 
+    goals_scored: u8, 
+    goals_conceded: u8
+)
+    {
+        let mut team = scores.entry(name.clone()).or_insert(Team::named(name));
+        team.goals_scored += goals_scored; 
+        team.goals_conceded += goals_conceded;
+    }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
@@ -35,8 +49,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        update_score(&mut scores,team_1_name,team_1_score,team_2_score);
+        update_score(&mut scores,team_2_name,team_2_score,team_1_score);
         // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
+        // current line. 
+
+        //Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
