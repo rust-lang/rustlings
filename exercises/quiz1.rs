c1fea5f1c6d36fa529911cba46c8c8d735b9442b
asif256000 <asif256000@gmail.com>
2022-12-22 13:07:24 -0500

    ✨ 🦀 Quiz Solved

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 {
+        count * 1
+    } else {
+        count * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
