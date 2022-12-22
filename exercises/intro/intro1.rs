ac478e46145bc5838abcfe583007de4d98699853
asif256000 <asif256000@gmail.com>
2022-12-21 21:51:04 -0500

    ✨ 🦀 Intro Solved

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -7,9 +7,7 @@
 //
 // If you're running this using `rustlings watch`: The exercise file will be reloaded
 // when you change one of the lines below! Try adding a `println!` line, or try changing
-// what it outputs in your terminal. Try removing a semicolon and see what happens!
-
-// I AM NOT DONE
+// what it outputs in your terminal. Try removing a semicolon and see what happens
 
 fn main() {
     println!("Hello and");
