eda16cf53482b797bb31a9a07f17799105c40042
asif256000 <asif256000@gmail.com>
2022-12-22 01:49:50 -0500

    ✨ 🦀 Variables Solved

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,10 +1,8 @@
 // variables4.rs
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
