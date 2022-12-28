9e6f99d90f6cd7e93ef4af834a6315010c07cc10
Karan Kadam <karank@outlook.com>
2022-12-28 11:28:11 +1030

    feat: 🎸 added solutions for macro exercises

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,14 +1,12 @@
 // macros2.rs
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    my_macro!();
 }
