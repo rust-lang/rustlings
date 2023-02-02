b2608c6ca7a998b3de870403e3c1441e4160d6e5
Michael Cain <mcain@taskrabbit.com>
2023-02-02 16:46:10 -0300

    Do exercises

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,8 +2,6 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!", "world");
 }
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,11 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    {
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
 }
