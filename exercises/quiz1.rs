0450a8c5b3e46379dc00463f06b961e49704c9ad
Shreyas Vaderiyattil <shreyasvaderiyattil@Shreyass-MacBook-Air.local>
2023-02-08 11:47:05 -0500

    25.5%

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        return 1 * quantity;
+    } else {
+        return 2 * quantity;
+    }
 
+ }
 // Don't modify this function!
 #[test]
 fn verify_test() {
