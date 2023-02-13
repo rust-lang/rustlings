b410ca2df76952db07020a3c8a776dd3db52fdad
Michael Cain <michael.m.cain.ii@gmail.com>
2023-02-13 09:57:50 -0600

    Update exercises/variables/variables4.rs
    Co-authored-by: Paul Scarrone <paul@scarrone.co>

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -2,8 +2,8 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    let mut x = 3;
+    let x = 3;
     println!("Number {}", x);
-    x = 5; // don't change this line
+    let x = 5; // don't change this line
     println!("Number {}", x);
 }
