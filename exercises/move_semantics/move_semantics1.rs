3f4d38c9fabe0b5298ec662b8a931d3bea3f0024
ben1009 <liuhe1009@gmail.com>
2023-02-14 16:43:14 +0800

    done with exercises/move_semantics/move_semantics1

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,11 @@
 // move_semantics1.rs
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(&mut vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -15,9 +14,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
