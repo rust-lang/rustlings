8b53ee2f0de0874df6b4c61a9cceeb4c39d536d4
Paul Scarrone <paul@scarrone.co>
2023-02-20 13:10:51 -0500

    functions 1

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+
+    fn hi() -> &'static str { "hi" }
+
+    fn call_me() {
+        println!("{}", hi())
+    }
+
     call_me();
 }
