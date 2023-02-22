033990da8396f3197e879e982c007755002c3c1f
mtricolici <mtricolici@inthergroup.com>
2023-02-22 09:53:18 +0200

    Progress

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,7 +9,6 @@
 // when you change one of the lines below! Try adding a `println!` line, or try changing
 // what it outputs in your terminal. Try removing a semicolon and see what happens!
 
-// I AM NOT DONE
 
 fn main() {
     println!("Hello and");
@@ -33,5 +32,5 @@ fn main() {
     println!(
         "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
     );
-    println!("up, and then run `rustlings lsp` before continuing.")
+    println!("up, and then run `rustlings lsp` before continuing.");
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(applesCount: i64) -> i64{
+    let price = if applesCount > 40 {1} else {2};
+    return applesCount * price;
+}
 
 // Don't modify this function!
 #[test]
