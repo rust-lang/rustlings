d6bf7355089530ef7f41aef062e62f0710da1962
sharkbyte <sgquin@gmail.com>
2023-02-24 17:12:10 -0800

    first few exercises and initial commit to my fork

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -7,5 +7,5 @@ fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
     number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    println!("Number plus two is : {}", number.parse() + 2);
 }
