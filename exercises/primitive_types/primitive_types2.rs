f7678b4a9ed03c81e2bedb998d5893f882acf915
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2023-02-27 12:57:40 +0100

    Learning about primitive data types

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -2,14 +2,12 @@
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
 
-// I AM NOT DONE
-
 fn main() {
     // Characters (`char`)
 
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
-    let my_first_initial = 'C';
+    let my_first_initial = 'N';
     if my_first_initial.is_alphabetic() {
         println!("Alphabetical!");
     } else if my_first_initial.is_numeric() {
@@ -18,12 +16,12 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
+    let my_favorite_character = '🔥'; // Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
-    if your_character.is_alphabetic() {
+    if my_favorite_character.is_alphabetic() {
         println!("Alphabetical!");
-    } else if your_character.is_numeric() {
+    } else if my_favorite_character.is_numeric() {
         println!("Numerical!");
     } else {
         println!("Neither alphabetic nor numeric!");
--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,10 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = ["Nidhal Messaoudi, A Rust professional!"; 100];
+
+    println!("{}", a.len());
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
