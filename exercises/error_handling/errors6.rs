da5b91c9991e6fbc7efd36bd7fd06fe8be92fcbd
asif256000 <asif256000@gmail.com>
2023-03-18 20:24:37 -0400

    ✨ 🦀 Errors Solved

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,9 +8,7 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-use std::num::ParseIntError;
+use std::{io::ErrorKind, num::ParseIntError};
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
@@ -25,13 +23,28 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    // let x: i64 = match s.parse().unwrap() {
+    //     Ok(x) => PositiveNonzeroInteger(x as u64),
+    //     Err(error) => match error.kind() {
+    //         CreationError => {
+    //             PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    //         }
+    //         ParseIntError => {
+    //             PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_parseint)
+    //         }
+    //     },
+    // };
+    // x
 }
 
 // Don't change anything below this line.
