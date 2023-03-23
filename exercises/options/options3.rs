6ec4caded5a8b34e8349d56b8b6fb7ebb22251d5
contre95 <lucascontre95@gmail.com>
2023-03-23 21:14:04 +0100

    Option3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -11,7 +11,7 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
