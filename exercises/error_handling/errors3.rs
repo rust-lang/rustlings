a28274789bddd3792cb2550ac7b557f014558adb
Abrar Habib <abrarhabib285@gmail.com>
2023-04-03 20:15:42 -0400

    reached generics2

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,22 +4,23 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
-
+    
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(()) // implicitly return an result at the end to satisfy return type
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,7 +1,6 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -15,7 +14,13 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value < 0 {
+            Err(CreationError::Negative)
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        } else {
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
 }
 
