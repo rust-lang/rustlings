bfe7974a5fa82a6e74ac7bdb309aaf4819201d51
Kacper Poneta <poneciak@gmail.com>
2023-04-05 22:10:13 +0200

    updated task

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,11 +1,19 @@
-// This shopping list program isn't compiling!
-// Use your knowledge of generics to fix it.
+// generics1.rs
+// Use your knowledge of generics to fix the function signature.
 
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
 
+fn last_on_list(list: &[&str]) -> &str {
+    list.last().unwrap()
+}
+
+// Do not change the main method
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    let names_list = vec!["maria", "jacob", "kacper"];
+    println!("last name on the list is: {}", last_on_list(&names_list));
+
+    let numbers_list = vec![1, 2, 3];
+    println!("last number on the list is: {}", last_on_list(&numbers_list));
 }
