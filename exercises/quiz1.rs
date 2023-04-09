92e4c21c0fa89288fc2097672cd55e8fa0eb37cb
karmabadger <karmabadger@outlook.com>
2023-04-09 02:56:51 -0400

    finished quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        num_apples
+    } else {
+        num_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
