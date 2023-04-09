54971f6aff2cd35172a19c4c64408f15884b938c
Yedidya Rashi <yedidyarashi@gmail.com>
2023-04-09 20:13:36 +0300

    feat: complete generics

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -3,9 +3,7 @@
 
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
