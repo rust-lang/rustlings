8bed288e0555603917a024b84cae8b89c8903fef
Yedidya Rashi <yedidyarashi@gmail.com>
2023-04-10 11:46:22 +0300

    feat: complete lifetimes

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,17 +4,18 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
-    let book = Book { author: &name, title: &title };
+    let book = Book {
+        author: &name,
+        title: &title,
+    };
 
     println!("{} by {}", book.title, book.author);
 }
