52d295027f681b87bbecde4ffa640ea847de40e7
Himan-Miku <himanshu28.mj@gmail.com>
2023-04-18 18:31:55 +0530

    move_semantics done ✅

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -6,12 +6,12 @@
 // vec0 has length 3 content `[22, 44, 66]`
 // vec1 has length 4 content `[22, 44, 66, 88]`
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
     let mut vec1 = fill_vec(vec0);
+
+    let vec0 = vec1.clone();
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
