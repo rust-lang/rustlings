6b9d1969ddd763886c5ce97d974df7d92d63282d
NamesMark <markfirn@gmail.com>
2023-05-12 02:17:31 +0300

    day 1

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,24 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+
+
+// // Using parsing:
+// fn main() {
+//     let mut number = "T-H-R-E-E"; // don't change this line
+//     println!("Spell a Number : {}", number);
+//     number = "3"; // don't rename this variable
+//     println!("Number plus two is : {}", number.parse::<i32>().unwrap() + 2);
+// }
+
+
+// Using shadowing:
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
-    println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    {
+        let number = "T-H-R-E-E"; // don't change this line
+        println!("Spell a Number : {}", number);
+    }
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
