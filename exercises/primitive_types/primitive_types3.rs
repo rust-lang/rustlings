5736a85736fa618a1bd8cd0165cc974c34e0efd5
Thomas Lockney <thomas@lockney.net>
2023-05-18 02:07:46 +0000

    Current state

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = [1; 200];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u32) -> u32 {
+    if count <= 40 {
+        count * 2
+    } else {
+        count
+    }
+}
 
 // Don't modify this function!
 #[test]
