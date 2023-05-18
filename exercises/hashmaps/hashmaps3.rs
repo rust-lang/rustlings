3707571685af21423849df668b7bd5760ce1d2ac
Hariettemaina <hariettemaina@gmail.com>
2023-05-18 16:34:30 +0300

    built scores table

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -25,6 +25,7 @@ struct Team {
     goals_conceded: u8,
 }
 
+
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
@@ -35,14 +36,28 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        
+        let team_1 = scores.entry(team_1_name.clone()).or_insert(Team {
+            name: team_1_name.clone(),
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+        
+        let team_2 = scores.entry(team_2_name.clone()).or_insert(Team {
+            name: team_2_name.clone(),
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
+    
     scores
 }
+
+
 
 #[cfg(test)]
 mod tests {
