d748cd156f03c4becee849cec24029f368fb1f27
Hariettemaina <hariettemaina@gmail.com>
2023-05-22 17:01:36 +0300

    adding else condtions

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -5,14 +5,14 @@
 // construct to `Option` that can be used to express error conditions. Let's use it!
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub fn generate_nametag_text(name: String) -> Option<String> {
+
+pub fn generate_nametag_text(name: String) -> Result<String,String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".into())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 
