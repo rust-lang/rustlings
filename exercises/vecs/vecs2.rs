d47d23ee8681b80e182083eaf31674dac092df0b
Hariettemaina <hariettemaina@gmail.com>
2023-05-25 11:03:29 +0300

    fixed the variable name to make vecs multiplied by 2

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -12,7 +12,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-       *i *= 2; 
+       *element *= 2; 
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -20,7 +20,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
+    v.iter().map(|&num| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
         num * 2
