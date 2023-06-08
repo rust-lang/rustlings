a4fe3602b238f38e9fd245874c863852791129fb
Bert Apperlo <91734527+b-apperlo@users.noreply.github.com>
2023-06-08 16:46:45 +0200

    fix: updated comment for struct

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -18,7 +18,7 @@
 
 use std::collections::HashMap;
 
-// A structure to store team name and its goal details.
+// A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
