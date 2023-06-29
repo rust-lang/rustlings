87d6f7402d39bb423313477e2f5c3dbce68bda55
antonio.calvin <garcia.calvin.toni@gmail.com>
2023-06-29 17:23:05 +0200

    Solves {Variables, Functions, If, Quiz} excercices

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    let price = if n_apples > 40 {1} else {2};
+    n_apples * price
+}
 
 // Don't modify this function!
 #[test]
