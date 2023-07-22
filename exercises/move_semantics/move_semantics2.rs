20000a457861b3f03ada7ed4e4e0b56e6cfb7f3b
palutz <>
2023-07-22 22:54:04 +0100

    move semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,15 +7,12 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
-
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
+    let mut vec1 = fill_vec(vec0);
     vec1.push(88);
 
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
