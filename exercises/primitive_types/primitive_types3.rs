5a5ae2417138c6224e03e02eae2070fc0663c793
Islam <iutizev@gmail.com>
2023-08-10 21:08:00 +0000

    repite 1

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = [0; 101];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
