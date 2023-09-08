a5e41335161601acc3e86e0b9425c66c9af354c3
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2023-09-08 16:42:16 +0200

    Make primitive_types3 require at least 100 elements
    Made the function panic if it's not long enough

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -14,5 +14,6 @@ fn main() {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed")
     }
 }
