6e66ff1f83401ee232c4d13568a6241ba60f977f
Daniel Baraniak <dbaraniak2000@gmail.com>
2023-09-11 00:32:45 +0200

    traits

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,11 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        "Some information".to_owned()
+    }
 }
 
 struct SomeSoftware {
