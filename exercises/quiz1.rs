e8961a89dec63a37a7242f3640806c67654654b3
stephan.klauberg <stephan.klauberg@mailbox.org>
2023-09-23 18:16:12 +0200

    quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(amount:i32)->i32 {
+    if (amount>40) {
+        amount
+    } else {
+        amount*2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
