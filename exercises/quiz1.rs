4057b47ab7cb42308ab1390a05b0e1e77211f03b
Jochen Zimmer <jochen.zimmer@gmail.com>
2023-10-11 22:00:59 +0200

    rustlings rust exercises up until primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
-// Put your function here!
-// fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(count:i32) -> i32{
+    if count <= 40 {
+        count * 2
+    }else{
+        count * 1
+    }
+}
 
 // Don't modify this function!
 #[test]
