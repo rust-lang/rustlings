3b0fc5b2652680f43c8a696cf14888cc345cead7
PattyC <patrickcoffey91@gmail.com>
2023-10-16 14:24:09 +0930

    up to quiz 1

--- exercises/if/if3.rs
+++ exercises/if/if3.rs
@@ -2,17 +2,15 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        -1
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
