13141c2c56325550eab3a8770981d04a6a64af53
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-10-21 19:33:49 +0530

    intro completed

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -5,8 +5,8 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    let w ="world";
+    println!("Hello {w}!");
 }
