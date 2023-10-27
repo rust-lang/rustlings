0ab8e329907b1bdfed6ecf3d373546dcf908b17c
mDanone <daniyalbek.daniyalov@gmail.com>
2023-10-27 23:39:39 +0300

    examples

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -11,7 +11,7 @@
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = a[::3];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(price: i32) -> i32 {
+    if price <= 40{
+        price * 2
+    } else {
+        price
+    }
+}
 
 // Don't modify this function!
 #[test]
