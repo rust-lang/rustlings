1b15a41671be0bd77861d94645f6754ce59e83ec
akshitgautam42 <akshitgautam42@gmail.com>
2023-11-12 14:32:24 +0530

    Quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n:i32) -> i32 {
+    if n==35 || n==40 {
+        return n*2
+    }
+    return n
+}
 
 // Don't modify this function!
 #[test]
