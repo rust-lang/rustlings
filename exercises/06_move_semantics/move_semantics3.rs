3bb525248efe7995c83441ba2640e4925914d84f
akshitgautam42 <akshitgautam42@gmail.com>
2023-11-12 15:01:16 +0530

    Exercise 27

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -12,12 +12,12 @@
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    let  vec1 = fill_vec(vec0.clone());
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
