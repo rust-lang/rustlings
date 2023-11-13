1b393bbe1104fd325bdeef0bc0d47c91e63575a3
akshitgautam42 <akshitgautam42@gmail.com>
2023-11-13 18:03:32 +0530

    Exercise 39

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -5,17 +5,17 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
+fn is_a_color_word(attempt: &String) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
