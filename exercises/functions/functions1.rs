a518fda83aa6d3d5c65b31e069e154f9ed53ebda
Marco Cauzzi <furai93@gmail.com>
2023-11-25 16:50:22 +0100

    Esercizi risolti 33/96

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,8 +3,10 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     call_me();
+}
+fn call_me(){
+
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -30,4 +29,11 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+fn calculate_price_of_apples(value:i32)->i32{
+    if(value>40){
+        return value;
+    }else{
+         return value*2;
+    }
 }
