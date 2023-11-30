ccd816d57ff43abd544c897cbe0fcb62d46a96e4
Faiizan Hussain <67575585+faiizanhussain@users.noreply.github.com>
2023-11-30 19:41:07 +0000

    Variables exercise completed

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -3,11 +3,10 @@
 // Make me compile!
 //
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
