0d87ae21094ab17df2313007303efd4002799efe
asif256000 <asif256000@gmail.com>
2023-12-14 16:43:26 +0530

    ✨ 🦀 Traits Solved

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -7,10 +7,10 @@
 // Consider what you can add to the Licensed trait.
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
