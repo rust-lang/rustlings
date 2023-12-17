778452a921a0738a87f798b12f8dc4d21f9f30ff
Rock070 <a0938130702@gmail.com>
2023-12-17 16:37:19 +0800

    Refactor function parameters in move_semantics6.rs

--- exercises/06_move_semantics/move_semantics6.rs
+++ exercises/06_move_semantics/move_semantics6.rs
@@ -5,24 +5,22 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data); // 修改這裡，傳遞一個參考
 
-    string_uppercase(&data);
+    string_uppercase(data); // `data` 的所有權被移動到 `string_uppercase`
 }
 
-// Should not take ownership
-fn get_char(data: String) -> char {
+// 現在 `get_char` 接收一個對 `String` 的不可變參考
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
-// Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+// `string_uppercase` 保持不變，因為它需要取得所有權
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
