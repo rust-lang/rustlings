69539ece4988839db765ab54086a927d1ca1688d
Eugen <eugen252009@gmail.com>
2023-12-19 01:10:51 +0100

    Auf Branch learning_rust  Zum Commit vorgemerkte Änderungen: 	geändert:       exercises/00_intro/intro1.rs 	geändert:       exercises/00_intro/intro2.rs 	geändert:       exercises/01_variables/variables1.rs 	geändert:       exercises/01_variables/variables2.rs 	geändert:       exercises/01_variables/variables3.rs 	geändert:       exercises/01_variables/variables4.rs 	geändert:       exercises/01_variables/variables5.rs 	geändert:       exercises/01_variables/variables6.rs 	geändert:       exercises/02_functions/functions1.rs 	geändert:       exercises/02_functions/functions2.rs 	geändert:       exercises/02_functions/functions3.rs 	geändert:       exercises/02_functions/functions4.rs 	geändert:       exercises/02_functions/functions5.rs 	geändert:       exercises/03_if/if1.rs 	geändert:       exercises/03_if/if2.rs 	geändert:       exercises/03_if/if3.rs 	geändert:       exercises/04_primitive_types/primitive_types1.rs 	geändert:       exercises/04_primitive_types/primitive_types2.rs 	geändert:       exercises/04_primitive_types/primitive_types3.rs 	geändert:       exercises/04_primitive_types/primitive_types4.rs 	geändert:       exercises/04_primitive_types/primitive_types5.rs 	geändert:       exercises/04_primitive_types/primitive_types6.rs 	geändert:       exercises/05_vecs/vecs1.rs 	geändert:       exercises/05_vecs/vecs2.rs 	geändert:       exercises/06_move_semantics/move_semantics1.rs 	geändert:       exercises/06_move_semantics/move_semantics2.rs 	geändert:       exercises/06_move_semantics/move_semantics3.rs 	geändert:       exercises/06_move_semantics/move_semantics4.rs 	geändert:       exercises/06_move_semantics/move_semantics5.rs 	geändert:       exercises/06_move_semantics/move_semantics6.rs 	geändert:       exercises/07_structs/structs1.rs 	geändert:       exercises/07_structs/structs2.rs 	geändert:       exercises/quiz1.rs 	neue Datei:     rustlings

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -5,13 +5,11 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(vec0.clone());
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Order {
@@ -20,13 +19,13 @@ struct Order {
 
 fn create_order_template() -> Order {
     Order {
-        name: String::from("Bob"),
+        name: String::from("Hacker in Rust"),
         year: 2019,
         made_by_phone: false,
         made_by_mobile: false,
         made_by_email: true,
         item_number: 123,
-        count: 0,
+        count: 1,
     }
 }
 
@@ -38,7 +37,15 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+            name: order_template.name,
+            year: order_template.year,
+            made_by_phone: order_template.made_by_phone,
+            made_by_mobile: order_template.made_by_mobile,
+            made_by_email: order_template.made_by_email,
+            item_number: order_template.item_number,
+            count: order_template.count,
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
