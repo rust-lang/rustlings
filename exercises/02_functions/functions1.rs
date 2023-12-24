91cdb8b6dfc7ff14dae0b3f747716303465af310
Xiaokang Xin <xiaokang.xin@airbnb.com>
2023-12-24 12:00:55 -0800

    section 2

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,7 +3,9 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+fn call_me() {
+    println!("Hello, world!");
+}
 
 fn main() {
     call_me();
