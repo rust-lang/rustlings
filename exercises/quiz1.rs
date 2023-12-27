18509cba3f4142893956626c0303ac0c6c97b3fb
Artur Zinurov <arturzinurov971015@gmail.com>
2023-12-26 20:58:35 -0600

    quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(apple: i32) -> i32{
+    let price: i32 = if apple > 40 {
+        apple * 1
+    } else {
+        apple * 2
+    };
+    price
+
+ }
 
 // Don't modify this function!
 #[test]
