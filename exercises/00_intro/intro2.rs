4bd2ca16c46a1ccaccea57ed8b5ab4e2d0a5ff8a
horaoen <horaoen@gmail.com>
2023-12-27 18:12:03 +0800

    update

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -3,10 +3,7 @@
 // Make the code print a greeting to the world.
 //
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 fn main() {
-    printline!("Hello there!")
+    println!("Hello there!")
 }
