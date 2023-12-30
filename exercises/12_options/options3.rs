43f6907aa14344580192e75fab4f34fa2948da7e
Rock070 <a0938130702@gmail.com>
2023-12-31 03:25:21 +0800

    Fix match statement in options3.rs

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -13,9 +11,9 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    let y = match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
-    }
+    };
     y; // Fix without deleting this line.
 }
