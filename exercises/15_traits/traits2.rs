46060cf08fc02483cf22198532aa239f144b4404
Rock070 <a0938130702@gmail.com>
2024-01-03 00:16:38 +0800

    Implement AppendBar trait for Vec<String>

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -8,13 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(&mut self) -> Vec<String> {
+        let vec = self.to_vec().push(String::from("Bar"));
+
+        vec
+    }
+}
 
 #[cfg(test)]
 mod tests {
