ab8afb64d6bbe19a1c936f110f70832666ab01fb
Artur Zinurov <arturzinurov971015@gmail.com>
2024-01-05 01:09:35 -0600

    move is done

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -3,11 +3,10 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
-    let vec0 = vec![22, 44, 66];
+    let mut vec0 = vec![22, 44, 66];
 
     let vec1 = fill_vec(vec0);
 
@@ -15,7 +14,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
