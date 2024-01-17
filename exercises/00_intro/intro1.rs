d7ce72e95fe1ca762b051f43afa84feeb2208ddc
Nevin Aragam <nevin.aragam@gmail.com>
2024-01-17 00:10:26 -0700

    fix: solved 00_intro.

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -13,8 +13,6 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
