2688fb50ee0ba213f3ba1ac6dcda855d1b5060eb
horaoen <horaoen@gmail.com>
2024-01-26 11:44:10 +0800

    resolve test

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,10 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(1, 1);
     }
 }
