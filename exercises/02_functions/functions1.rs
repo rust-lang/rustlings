7cad97ec5bc5d8837a2512f5517f04dc8d2e5fef
Suzie Kim <suzie.kim@shopify.com>
2024-01-31 10:40:34 -0500

    Complete functions exercises

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,14 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
+    println!("You define functions outside of main and call them here.");
+    println!("Call me.");
+    // Rust doesn't care where the function is defined, as long as it's within the scope.
     call_me();
+}
+
+fn call_me() {
+    println!("Beep me. If you wanna reach me.");
 }
--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -11,5 +10,8 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // you can either add an explicit return to this expression
+    // or remove the semicolon to make it an implicit return
+    // you cannot just have an expression with a semicolon
+    return num * num;
 }
