fca2d54cb80e7f3b1cc9c7eac5fdbaab191b91b1
Suzie Kim <suzie.kim@shopify.com>
2024-01-31 11:15:37 -0500

    Complete Quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 { quantity } else { quantity * 2 }
+}
 
 // Don't modify this function!
 #[test]
