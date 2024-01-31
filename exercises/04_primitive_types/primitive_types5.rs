d3ae805794b79f1a47d7ce88fac7967e22e06acc
Suzie Kim <suzie.kim@shopify.com>
2024-01-31 11:38:14 -0500

    Complete primitives exercises

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -5,11 +5,13 @@
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    // Tuples are ways to group together values with different types
+    // they have fixed lengths - cannot grow or shrink
+    // considered a sinle compound and it can be destructured
+    // it can be more than 2 values at a time. 
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    let (name, age)/* your pattern here */ = cat;
 
     println!("{} is {} years old.", name, age);
 }
