34e542a471641cb173fafb4519f59739fd145e42
Suzie Kim <suzie.kim@shopify.com>
2024-02-02 15:07:05 -0500

    Complete tests exercises

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,11 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let new_string = "test";
+        assert_eq!(new_string, "test");
     }
 }
--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -19,11 +19,11 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(2));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(5));
     }
 }
