5e3230c931e080a23617f30dc3fb45d7bc86a614
Suzie Kim <suzie.kim@shopify.com>
2024-02-02 14:31:41 -0500

    Complete lifetimes exercises

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,16 +5,18 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
+
+    // The Book struct is returning two parameters
+    // so it must specify the lifetime of both references.
     let book = Book { author: &name, title: &title };
 
     println!("{} by {}", book.title, book.author);
