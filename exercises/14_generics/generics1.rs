a6ab524725b84b3037361489bc59da48a9d764c8
Suzie Kim <suzie.kim@shopify.com>
2024-02-02 10:33:50 -0500

    Complete generics exercises

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -6,9 +6,19 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
+
+
+// fn main() {
+//     let mut shopping_list: Vec<&str> = Vec::new();
+//     // shopping_list.push("milk");
+//     add_to_shopping_list(shopping_list, "milk")
+// }
+
+// fn add_to_shopping_list<T>(mut list: Vec<T>, grocery: T) {
+//     list.push(grocery);
+// }
