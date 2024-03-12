da7d78fb7b3c49b9f57f97fd0cda692ac6b73eb9
Raihan Luthfi <raihan.luthfi.h@gmail.com>
2024-03-12 17:59:06 +0700

    add answers for quiz1 and primitive_types section

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num <= 40 {
+        num * 2
+    } else {
+        num
+    }
+}
 
 // Don't modify this function!
 #[test]
