d8ecf4bc2d34a10149999a7974d5ba71625fab90
liv <shadows_withal@pm.me>
2024-03-15 15:01:32 +0100

    fix: clean up "return" wording in iterators4

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -8,7 +8,7 @@
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
-    // - return
+    // - early returns (using the `return` keyword explicitly)
     // Try not to use:
     // - imperative style loops (for, while)
     // - additional variables
