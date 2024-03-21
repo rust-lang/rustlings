2542b5d2495b4d1a255a91ac1e35baf1c24a01f1
laazy <laaazy@sjtu.edu.cn>
2024-03-21 22:26:17 +0800

    🎉 complete quiz1!

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_me(2);
 }
 
 fn call_me(num: u32) {
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: u32) -> u32 {
+    if apple_count > 40 {
+        apple_count
+    } else {
+        apple_count * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
