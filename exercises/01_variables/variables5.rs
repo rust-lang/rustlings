99b032cb81f43c28f8ab8fa885c87550c51f0378
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-03-21 13:37:05 +0000

    Pending changes exported from your codespace

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -8,6 +8,6 @@
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
