9099bdcea5cc58b964d3e849a074df4bdb4eeed4
laazy <laaazy@sjtu.edu.cn>
2024-03-23 14:10:41 +0800

    🚧 temp save

--- exercises/19_smart_pointers/cow1.rs
+++ exercises/19_smart_pointers/cow1.rs
@@ -48,7 +48,8 @@ mod tests {
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Borrowed(_) => Ok(()),
+            Cow::Owned(_) => Err("Expect"),
         }
     }
 
@@ -60,7 +61,8 @@ mod tests {
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            Cow::Borrowed(_) => Err("Expected owned value"),
         }
     }
 
@@ -72,7 +74,8 @@ mod tests {
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            Cow::Borrowed(_) => Err(""),
         }
     }
 }
