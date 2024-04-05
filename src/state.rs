de9a0ed5221934b43a27921455f484e006c3ec20
mo8it <mo8it@proton.me>
2024-04-06 01:46:22 +0200

    Update state

--- src/state.rs
+++ src/state.rs
@@ -1,31 +1,37 @@
 use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
-use std::{fs, io, path::PathBuf};
+use std::fs;
 
-#[derive(Serialize, Deserialize)]
-pub struct ExerciseState {
-    pub path: PathBuf,
-    pub done: bool,
-}
+use crate::exercise::Exercise;
 
 #[derive(Serialize, Deserialize)]
 pub struct State {
-    pub progress: Vec<ExerciseState>,
+    pub progress: Vec<bool>,
 }
 
 impl State {
-    pub fn read() -> Result<Self> {
-        let file_content =
-            fs::read(".rustlings.json").context("Failed to read the file `.rustlings.json`")?;
+    fn read(exercises: &[Exercise]) -> Option<Self> {
+        let file_content = fs::read(".rustlings.json").ok()?;
 
-        serde_json::de::from_slice(&file_content)
-            .context("Failed to deserialize the file `.rustlings.json`")
+        let slf: Self = serde_json::de::from_slice(&file_content).ok()?;
+
+        if slf.progress.len() != exercises.len() {
+            return None;
+        }
+
+        Some(slf)
     }
 
-    pub fn write(&self) -> io::Result<()> {
+    pub fn read_or_default(exercises: &[Exercise]) -> Self {
+        Self::read(exercises).unwrap_or_else(|| Self {
+            progress: vec![false; exercises.len()],
+        })
+    }
+
+    pub fn write(&self) -> Result<()> {
         // TODO: Capacity
         let mut buf = Vec::with_capacity(1 << 12);
-        serde_json::ser::to_writer(&mut buf, self).context("Failed to serialize the state");
+        serde_json::ser::to_writer(&mut buf, self).context("Failed to serialize the state")?;
         dbg!(buf.len());
         Ok(())
     }
