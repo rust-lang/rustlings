86d716cf8a59092ba2078f2b0d80f95e155f2d64
mo8it <mo8it@proton.me>
2024-04-16 03:43:34 +0200

    Add comment about keeping dependencies

--- src/dev/check.rs
+++ src/dev/check.rs
@@ -7,6 +7,7 @@ use crate::{info_file::InfoFile, init::cargo_toml};
 pub fn check(info_file: InfoFile) -> Result<()> {
     // TODO: Add checks
 
+    // TODO: Keep dependencies!
     fs::write("Cargo.toml", cargo_toml(&info_file.exercises))
         .context("Failed to update the file `Cargo.toml`")?;
     println!("Updated `Cargo.toml`");
