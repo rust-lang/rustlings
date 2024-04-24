c45ba8f15274dd08fd49a1561650c2631256143f
Andrew Kushyk <61907955+git-user-cpp@users.noreply.github.com>
2024-04-24 07:07:45 +0000

    if statements

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -2,14 +2,15 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
-    // Complete this function to return the bigger number!
-    // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
+    if a > b {
+        return a;
+    } else if a < b {
+        return b;
+    }
+
+    a
 }
 
 // Don't mind this for now :)
