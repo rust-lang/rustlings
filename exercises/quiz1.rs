db809985ae012f1e70eb8ed0164b8cad2c9c13d4
Andrew Kushyk <61907955+git-user-cpp@users.noreply.github.com>
2024-04-24 07:12:52 +0000

    primitives

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    if apples > 40 {
+        return apples;
+    }
+
+    apples * 2
+}
 
 // Don't modify this function!
 #[test]
