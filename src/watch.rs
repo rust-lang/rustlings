c7c8d9968040d7df438fa33b32d9495415f751d2
mo8it <mo8it@proton.me>
2024-04-25 15:22:11 +0200

    Moar responsive :P

--- src/watch.rs
+++ src/watch.rs
@@ -51,7 +51,7 @@ pub fn watch(
     // Otherwise, the file watcher exits.
     let _debouncer_guard = if let Some(exercise_names) = notify_exercise_names {
         let mut debouncer = new_debouncer(
-            Duration::from_millis(500),
+            Duration::from_millis(200),
             DebounceEventHandler {
                 tx: tx.clone(),
                 exercise_names,
