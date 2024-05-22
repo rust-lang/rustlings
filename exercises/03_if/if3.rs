73e84f83791f00ef8ccfe438bc018d2c0a9b21fe
mo8it <mo8it@proton.me>
2024-05-22 15:54:35 +0200

    if3 solution

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,4 +1,5 @@
-fn animal_habitat(animal: &str) -> &'static str {
+fn animal_habitat(animal: &str) -> &str {
+    // TODO: Fix the compiler error in the statement below.
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
@@ -9,8 +10,8 @@ fn animal_habitat(animal: &str) -> &'static str {
         "Unknown"
     };
 
-    // DO NOT CHANGE THIS STATEMENT BELOW
-    let habitat = if identifier == 1 {
+    // Don't change the expression below!
+    if identifier == 1 {
         "Beach"
     } else if identifier == 2 {
         "Burrow"
@@ -18,37 +19,35 @@ fn animal_habitat(animal: &str) -> &'static str {
         "Desert"
     } else {
         "Unknown"
-    };
-
-    habitat
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
 }
 
-// No test changes needed.
+// Don't change the tests!
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn gopher_lives_in_burrow() {
-        assert_eq!(animal_habitat("gopher"), "Burrow");
+        assert_eq!(animal_habitat("gopher"), "Burrow")
     }
 
     #[test]
     fn snake_lives_in_desert() {
-        assert_eq!(animal_habitat("snake"), "Desert");
+        assert_eq!(animal_habitat("snake"), "Desert")
     }
 
     #[test]
     fn crab_lives_on_beach() {
-        assert_eq!(animal_habitat("crab"), "Beach");
+        assert_eq!(animal_habitat("crab"), "Beach")
     }
 
     #[test]
     fn unknown_animal() {
-        assert_eq!(animal_habitat("dinosaur"), "Unknown");
+        assert_eq!(animal_habitat("dinosaur"), "Unknown")
     }
 }
