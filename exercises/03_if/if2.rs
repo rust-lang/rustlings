eafb157d60ee46e95e2b54ec75b33180a838b0c5
mo8it <mo8it@proton.me>
2024-05-22 15:16:50 +0200

    if2 solution

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,6 +1,4 @@
-// Step 1: Make me compile!
-// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
-
+// TODO: Fix the compiler error on this function.
 fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
@@ -13,13 +11,15 @@ fn main() {
     // You can optionally experiment here.
 }
 
-// No test changes needed!
+// TODO: Read the tests to understand the desired behavior.
+// Make all tests pass without changing them.
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn foo_for_fizz() {
+        // This means that calling `foo_if_fizz` with the argument "fizz" should return "foo".
         assert_eq!(foo_if_fizz("fizz"), "foo");
     }
 
--- solutions/03_if/if2.rs
+++ solutions/03_if/if2.rs
@@ -1,1 +1,33 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn foo_if_fizz(fizzish: &str) -> &str {
+    if fizzish == "fizz" {
+        "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
+    } else {
+        "baz"
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo_for_fizz() {
+        assert_eq!(foo_if_fizz("fizz"), "foo");
+    }
+
+    #[test]
+    fn bar_for_fuzz() {
+        assert_eq!(foo_if_fizz("fuzz"), "bar");
+    }
+
+    #[test]
+    fn default_to_baz() {
+        assert_eq!(foo_if_fizz("literally anything"), "baz");
+    }
+}
