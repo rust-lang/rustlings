0338b1cbdfa567d5f9580afef1d4483c7d275c32
mo8it <mo8it@proton.me>
2024-06-08 21:43:38 +0200

    primitive_types3 solution

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,12 +1,11 @@
-// Create an array with at least 100 elements in it where the ??? is.
-
 fn main() {
-    let a = ???
+    // TODO: Create an array with at least 100 elements in it where the ??? is.
+    // let a = ???
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
-        panic!("Array not big enough, more elements needed")
+        panic!("Array not big enough, more elements needed");
     }
 }
