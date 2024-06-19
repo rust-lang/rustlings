a9f0c7bf1f00ab19733953d3121d462eede34466
mo8it <mo8it@proton.me>
2024-06-20 01:00:06 +0200

    vecs1 solution

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -1,11 +1,9 @@
-// Your task is to create a `Vec` which holds the exact same elements as in the
-// array `a`.
-//
-// Make me compile and pass the test!
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let a = [10, 20, 30, 40]; // Array
+
+    // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
+    // Use the vector macro.
+    // let v = ???;
 
     (a, v)
 }
@@ -21,6 +19,6 @@ mod tests {
     #[test]
     fn test_array_and_vec_similarity() {
         let (a, v) = array_and_vec();
-        assert_eq!(a, v[..]);
+        assert_eq!(a, *v);
     }
 }
--- solutions/05_vecs/vecs1.rs
+++ solutions/05_vecs/vecs1.rs
@@ -1,1 +1,23 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // Array
+
+    // Used the `vec!` macro.
+    let v = vec![10, 20, 30, 40];
+
+    (a, v)
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, *v);
+    }
+}
