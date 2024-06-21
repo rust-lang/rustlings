946c29679e27433ff455bdb30343551757d87769
mo8it <mo8it@proton.me>
2024-06-21 16:16:52 +0200

    move_semantics1 solution

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,3 +1,4 @@
+// TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let vec = vec;
 
@@ -17,9 +18,7 @@ mod tests {
     #[test]
     fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
-
         let vec1 = fill_vec(vec0);
-
         assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
--- solutions/06_move_semantics/move_semantics1.rs
+++ solutions/06_move_semantics/move_semantics1.rs
@@ -1,1 +1,25 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+    //  ^^^ added
+
+    vec.push(88);
+
+    vec
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn move_semantics1() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(vec0);
+        // `vec0` can't be accessed anymore because it is moved to `fill_vec`.
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+}
