fd558065c7f32d38d1b8e34fda1a23fe40d1b3ab
mo8it <mo8it@proton.me>
2024-06-21 17:04:51 +0200

    move_semantics3 solution

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,6 +1,4 @@
-// Make me compile without adding new lines -- just changing existing lines! (no
-// lines with multiple semicolons necessary!)
-
+// TODO: Fix the compiler error in the function without adding any new line.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
@@ -18,9 +16,7 @@ mod tests {
     #[test]
     fn move_semantics3() {
         let vec0 = vec![22, 44, 66];
-
         let vec1 = fill_vec(vec0);
-
-        assert_eq!(vec1, vec![22, 44, 66, 88]);
+        assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
--- solutions/06_move_semantics/move_semantics3.rs
+++ solutions/06_move_semantics/move_semantics3.rs
@@ -1,1 +1,22 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    //      ^^^ added
+    vec.push(88);
+
+    vec
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn move_semantics3() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, [22, 44, 66, 88]);
+    }
+}
