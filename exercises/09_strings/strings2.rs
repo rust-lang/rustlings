f574905b8e7d3b9320b2cb3a4c18e2039c9a771f
mo8it <mo8it@proton.me>
2024-06-22 12:14:04 +0200

    strings2 solution

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -1,14 +1,14 @@
-// Make me compile without changing the function signature!
+// TODO: Fix the compiler error in the `main` function without changing this function.
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
 
 fn main() {
-    let word = String::from("green"); // Try not changing this line :)
+    let word = String::from("green"); // Don't change this line.
+
     if is_a_color_word(word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
-}
-
-fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
 }
--- solutions/09_strings/strings2.rs
+++ solutions/09_strings/strings2.rs
@@ -1,1 +1,15 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
+
+fn main() {
+    let word = String::from("green");
+
+    if is_a_color_word(&word) {
+        //             ^ added to have `&String` which is automatically
+        //               coerced to `&str` by the compiler.
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
