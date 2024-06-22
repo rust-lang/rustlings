ecbe9b7324364e94f7c6b4a4dd279fb90f5a938e
mo8it <mo8it@proton.me>
2024-06-22 13:12:39 +0200

    modules1 solution

--- exercises/10_modules/modules1.rs
+++ exercises/10_modules/modules1.rs
@@ -1,3 +1,4 @@
+// TODO: Fix the compiler error about calling a private function.
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
--- solutions/10_modules/modules1.rs
+++ solutions/10_modules/modules1.rs
@@ -1,1 +1,15 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+mod sausage_factory {
+    fn get_secret_recipe() -> String {
+        String::from("Ginger")
+    }
+
+    // Added `pub` before `fn` to make the function accessible outside the module.
+    pub fn make_sausage() {
+        get_secret_recipe();
+        println!("sausage!");
+    }
+}
+
+fn main() {
+    sausage_factory::make_sausage();
+}
