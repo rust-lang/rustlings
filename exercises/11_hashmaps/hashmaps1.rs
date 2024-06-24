5baa503bfc27fc691dbc292b46d37d25c17cffab
mo8it <mo8it@proton.me>
2024-06-24 13:20:50 +0200

    hashmaps1 solution

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -1,20 +1,19 @@
 // A basket of fruits in the form of a hash map needs to be defined. The key
 // represents the name of the fruit and the value represents how many of that
-// particular fruit is in the basket. You have to put at least three different
+// particular fruit is in the basket. You have to put at least 3 different
 // types of fruits (e.g apple, banana, mango) in the basket and the total count
-// of all the fruits should be at least five.
-//
-// Make me compile and pass the tests!
+// of all the fruits should be at least 5.
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    // TODO: Declare the hash map.
+    // let mut basket =
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
-    // TODO: Put more fruits in your basket here.
+    // TODO: Put more fruits in your basket.
 
     basket
 }
