097f3c74ea16bad95a659fc41a494f24e07656d1
mo8it <mo8it@proton.me>
2024-06-26 15:06:29 +0200

    errors1 solution

--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -1,20 +1,20 @@
-// This function refuses to generate text to be printed on a nametag if you pass
-// it an empty string. It'd be nicer if it explained what the problem was,
-// instead of just sometimes returning `None`. Thankfully, Rust has a similar
-// construct to `Option` that can be used to express error conditions. Let's use
-// it!
-
-fn main() {
-    // You can optionally experiment here.
-}
-
+// TODO: This function refuses to generate text to be printed on a nametag if
+// you pass it an empty string. It'd be nicer if it explained what the problem
+// was instead of just returning `None`. Thankfully, Rust has a similar
+// construct to `Option` that can be used to express error conditions. Change
+// the function signature and body to return `Result<String, String>` instead
+// of `Option<String>`.
 fn generate_nametag_text(name: String) -> Option<String> {
     if name.is_empty() {
         // Empty names aren't allowed.
         None
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Some(format!("Hi! My name is {name}"))
     }
+}
+
+fn main() {
+    // You can optionally experiment here.
 }
 
 #[cfg(test)]
@@ -24,17 +24,17 @@ mod tests {
     #[test]
     fn generates_nametag_text_for_a_nonempty_name() {
         assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
-            Ok("Hi! My name is Beyoncé".into())
+            generate_nametag_text("Beyoncé".to_string()).as_deref(),
+            Ok("Hi! My name is Beyoncé"),
         );
     }
 
     #[test]
     fn explains_why_generating_nametag_text_fails() {
         assert_eq!(
-            generate_nametag_text("".into()),
+            generate_nametag_text(String::new()).as_deref(),
             // Don't change this line
-            Err("`name` was empty; it must be nonempty.".into())
+            Err("`name` was empty; it must be nonempty."),
         );
     }
 }
--- solutions/13_error_handling/errors1.rs
+++ solutions/13_error_handling/errors1.rs
@@ -1,1 +1,35 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn generate_nametag_text(name: String) -> Result<String, String> {
+    //                                    ^^^^^^         ^^^^^^
+    if name.is_empty() {
+        // `Err(String)` instead of `None`.
+        Err("Empty names aren't allowed".to_string())
+    } else {
+        // `Ok` instead of `Some`.
+        Ok(format!("Hi! My name is {name}"))
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".to_string()).as_deref(),
+            Ok("Hi! My name is Beyoncé"),
+        );
+    }
+
+    #[test]
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text(String::new()).as_deref(),
+            Err("`name` was empty; it must be nonempty."),
+        );
+    }
+}
