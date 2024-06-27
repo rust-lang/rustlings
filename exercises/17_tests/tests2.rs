803e32dad2395d309b74b9fde6b9e08577cf8a0a
mo8it <mo8it@proton.me>
2024-06-27 16:40:26 +0200

    tests2 solution

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -1,5 +1,8 @@
-// This test has a problem with it -- make the test compile! Make the test pass!
-// Make the test fail!
+// Calculates the power of 2 using a bit shift.
+// `1 << n` is equivalent to "2 to the power of n".
+fn power_of_2(n: u8) -> u64 {
+    1 << n
+}
 
 fn main() {
     // You can optionally experiment here.
@@ -7,8 +10,14 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert_eq() {
+        // TODO: Test the function `power_of_2` with some values.
+        assert_eq!();
+        assert_eq!();
+        assert_eq!();
         assert_eq!();
     }
 }
