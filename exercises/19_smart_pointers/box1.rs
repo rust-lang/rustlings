61c7eaed6251fb8a28b00ea97b22d1f1b778a72b
mo8it <mo8it@proton.me>
2024-06-28 21:24:35 +0200

    box1 solution

--- exercises/19_smart_pointers/box1.rs
+++ exercises/19_smart_pointers/box1.rs
@@ -4,36 +4,34 @@
 // `Box` - a smart pointer used to store data on the heap, which also allows us
 // to wrap a recursive type.
 //
-// The recursive type we're implementing in this exercise is the `cons list` - a
+// The recursive type we're implementing in this exercise is the "cons list", a
 // data structure frequently found in functional programming languages. Each
-// item in a cons list contains two elements: the value of the current item and
+// item in a cons list contains two elements: The value of the current item and
 // the next item. The last item is a value called `Nil`.
-//
-// Step 1: use a `Box` in the enum definition to make the code compile
-// Step 2: create both empty and non-empty cons lists by replacing `todo!()`
-//
-// Note: the tests should not be changed
 
+// TODO: Use a `Box` in the enum definition to make the code compile.
 #[derive(PartialEq, Debug)]
 enum List {
     Cons(i32, List),
     Nil,
 }
 
-fn main() {
-    println!("This is an empty cons list: {:?}", create_empty_list());
-    println!(
-        "This is a non-empty cons list: {:?}",
-        create_non_empty_list()
-    );
-}
-
+// TODO: Create an empty cons list.
 fn create_empty_list() -> List {
     todo!()
 }
 
+// TODO: Create a non-empty cons list.
 fn create_non_empty_list() -> List {
     todo!()
+}
+
+fn main() {
+    println!("This is an empty cons list: {:?}", create_empty_list());
+    println!(
+        "This is a non-empty cons list: {:?}",
+        create_non_empty_list(),
+    );
 }
 
 #[cfg(test)]
@@ -42,7 +40,7 @@ mod tests {
 
     #[test]
     fn test_create_empty_list() {
-        assert_eq!(List::Nil, create_empty_list());
+        assert_eq!(create_empty_list(), List::Nil);
     }
 
     #[test]
