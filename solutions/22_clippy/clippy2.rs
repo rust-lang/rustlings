a0e810b4713bcef60f64f4709ee27c3acec676cd
mo8it <mo8it@proton.me>
2024-07-01 11:55:18 +0200

    clippy2 solution

--- solutions/22_clippy/clippy2.rs
+++ solutions/22_clippy/clippy2.rs
@@ -1,1 +1,10 @@
-// Solutions will be available before the stable release. Thank you for testing the beta version 🥰
+fn main() {
+    let mut res = 42;
+    let option = Some(12);
+    // Use `if-let` instead of iteration.
+    if let Some(x) = option {
+        res += x;
+    }
+
+    println!("{res}");
+}
