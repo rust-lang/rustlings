78728d52387730300475cbe8c83497f603a14faf
mo8it <mo8it@proton.me>
2024-07-01 11:54:35 +0200

    clippy1 solution

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -1,19 +1,17 @@
 // The Clippy tool is a collection of lints to analyze your code so you can
 // catch common mistakes and improve your Rust code.
 //
-// For these exercises the code will fail to compile when there are Clippy
+// For these exercises, the code will fail to compile when there are Clippy
 // warnings. Check Clippy's suggestions from the output to solve the exercise.
 
-use std::f32;
+use std::f32::consts::PI;
 
 fn main() {
-    let pi = 3.14f32;
-    let radius = 5.00f32;
+    // Use the more accurate `PI` constant.
+    let pi = PI;
+    let radius: f32 = 5.0;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = pi * radius.powi(2);
 
-    println!(
-        "The area of a circle with radius {:.2} is {:.5}!",
-        radius, area
-    )
+    println!("The area of a circle with radius {radius:.2} is {area:.5}");
 }
