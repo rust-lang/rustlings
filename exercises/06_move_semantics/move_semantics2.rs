b8c80e64152b3af486157f22cb05b70b42db06ad
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-07-03 12:25:38 +0800

    Update move_semantics2.rs

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -10,7 +10,7 @@
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(vec0.clone());
+    let vec1 = fill_vec(vec0);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
