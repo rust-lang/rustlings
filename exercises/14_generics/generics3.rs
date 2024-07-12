52af0674c1e2e306b8af8e6abf06074eb028e9fc
Kacper Poneta <poneciak@gmail.com>
2024-07-12 18:14:40 +0200

    changed the task to make it more appropriate

--- exercises/14_generics/generics3.rs
+++ exercises/14_generics/generics3.rs
@@ -1,14 +1,13 @@
 // generics3.rs
 // Execute `rustlings hint generics3` or use the `hint` watch subcommand for a hint.
 
-// TODO Use your knowledge of generics to enchance the `stringify` function by only changing the signature.
-fn stringify(list: &[&str]) -> String {
-    let items_str = list
-        .iter()
-        .map(|e| e.to_string())
-        .collect::<Vec<String>>()
-        .join(", ");
-    format!("[{items_str}]")
+// This function should take an array of `Option` elements and returns array of not None elements
+// TODO fix this function signature
+fn into_dispose_nulls(list: Vec<Option<&str>>) -> Vec<&str> {
+    list
+        .into_iter()
+        .flatten()
+        .collect()
 }
 
 fn main() {
@@ -21,15 +20,35 @@ mod tests {
 
     #[test]
     fn store_str_on_list() {
-        let names_list = vec!["maria", "jacob", "kacper"];
-        let stringified = stringify(&names_list);
-        assert_eq!(stringified, "[maria, jacob, kacper]".to_string());
+        let names_list = vec![Some("maria"), Some("jacob"), None, Some("kacper"), None];
+        let only_values = into_dispose_nulls(names_list);
+        assert_eq!(only_values.len(), 3);
     }
 
     #[test]
     fn store_numbers_on_list() {
-        let numbers_list = vec![1, 2, 3];
-        let stringified = stringify(&numbers_list);
-        assert_eq!(stringified, "[1, 2, 3]".to_string());
+        let numbers_list = vec![Some(1), Some(2), None, Some(3)];
+        let only_values = into_dispose_nulls(numbers_list);
+        assert_eq!(only_values.len(), 3);
     }
+
+    #[test]
+    fn store_custom_type_on_list() {
+        #[allow(dead_code)]
+        struct Rectangle {
+            width: i32,
+            height: i32
+        }
+        impl Rectangle {
+            fn new(width: i32, height: i32) -> Self {
+                Self { width, height }
+            }
+        }
+
+        let custom_list = vec![Some(Rectangle::new(1, 2)), None, None, Some(Rectangle::new(3, 4))];
+        let only_values = into_dispose_nulls(custom_list);
+        assert_eq!(only_values.len(), 2);
+    }
+
+
 }
--- solutions/14_generics/generics3.rs
+++ solutions/14_generics/generics3.rs
@@ -1,17 +1,13 @@
 // generics3.rs
 // Execute `rustlings hint generics3` or use the `hint` watch subcommand for a hint.
 
-// Here we add generic in function declaration so function can work with different types
-fn stringify<T>(list: &[T]) -> String 
-where
-    T: ToString // here we also specify that T needs to implement ToString trait so we can use .to_string() on the vetor elements
-{
-    let items_str = list
-        .iter()
-        .map(|e| e.to_string())
-        .collect::<Vec<String>>()
-        .join(", ");
-    format!("[{items_str}]")
+// Here we added generic type `T` to function signature
+// Now this function can be used with vector of any
+fn into_dispose_nulls<T>(list: Vec<Option<T>>) -> Vec<T> {
+    list
+        .into_iter()
+        .flatten()
+        .collect()
 }
 
 fn main() {
@@ -24,15 +20,32 @@ mod tests {
 
     #[test]
     fn store_str_on_list() {
-        let names_list = vec!["maria", "jacob", "kacper"];
-        let stringified = stringify(&names_list);
-        assert_eq!(stringified, "[maria, jacob, kacper]".to_string());
+        let names_list = vec![Some("maria"), Some("jacob"), None, Some("kacper"), None];
+        let only_values = into_dispose_nulls(names_list);
+        assert_eq!(only_values.len(), 3);
     }
 
     #[test]
     fn store_numbers_on_list() {
-        let numbers_list = vec![1, 2, 3];
-        let stringified = stringify(&numbers_list);
-        assert_eq!(stringified, "[1, 2, 3]".to_string());
+        let numbers_list = vec![Some(1), Some(2), None, Some(3)];
+        let only_values = into_dispose_nulls(numbers_list);
+        assert_eq!(only_values.len(), 3);
+    }
+
+    #[test]
+    fn store_custom_type_on_list() {
+        struct Rectangle {
+            width: i32,
+            height: i32
+        }
+        impl Rectangle {
+            fn new(width: i32, height: i32) -> Self {
+                Self { width, height }
+            }
+        }
+
+        let custom_list = vec![Some(Rectangle::new(1, 2)), None, None, Some(Rectangle::new(3, 4))];
+        let only_values = into_dispose_nulls(custom_list);
+        assert_eq!(only_values.len(), 2);
     }
 }
