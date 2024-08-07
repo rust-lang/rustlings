81bf0a64300a2dd1f05b71e2674cc927385df410
mo8it <mo8it@proton.me>
2024-08-07 23:46:11 +0200

    Remove redundant rustfmt check for solutions

--- release-hook.sh
+++ release-hook.sh
@@ -9,6 +9,5 @@ cargo upgrades
 # Similar to CI
 cargo clippy -- --deny warnings
 cargo fmt --all --check
-rustfmt --check --edition 2021 solutions/**/*.rs
 cargo test --workspace --all-targets
 cargo run -- dev check --require-solutions
