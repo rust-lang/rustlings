8016f5ca2db2d790c1046b2c57a1add3bcf9cf64
mo8it <mo8it@proton.me>
2024-08-17 14:55:58 +0200

    Remove unneeded comma

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -1,4 +1,4 @@
-// TODO: We sometimes encourage you to keep trying things on a given exercise,
+// TODO: We sometimes encourage you to keep trying things on a given exercise
 // even after you already figured it out. If you got everything working and feel
 // ready for the next exercise, enter `n` in the terminal.
 //
