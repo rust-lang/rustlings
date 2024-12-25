bfda4caf83f39bfc822a7f7b46b483f12b661a8f
GURUSUJAN REDDY <94339643+GURUSUJAN@users.noreply.github.com>
2024-12-26 01:53:42 +0530

    Updated TODO errors3.rs

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -20,12 +20,19 @@ fn main() {
     let pretend_user_input = "8";
 
     // Don't change this line.
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
 
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {tokens} tokens.");
+    match cost {
+      Ok(cost) => {
+        if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {tokens} tokens.");
+        }
+      },
+      Err(error) => {
+          println!("Error calculating the total cost: {}", error)
+      }
     }
 }
