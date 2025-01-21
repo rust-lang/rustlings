6a2638ff6e34a15c8e10dbc8fff033cfaae67eb0
bagus.cahyono <baguscah77@gmail.com>
2025-01-21 11:30:46 +0700

    fix: clippy error message not inline with the intent.

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -17,7 +17,8 @@ fn main() {
     ];
     println!("My array! Here it is: {my_arr:?}");
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.resize(0, 5);
     println!("This Vec is empty, see? {my_empty_vec:?}");
 
     let mut value_a = 45;
