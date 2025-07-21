b3d65eb80f58abedb9c5ff8e8f637b34d22ad773
Catheriany <44122226+Catheriany@users.noreply.github.com>
2025-07-21 14:58:54 +0800

    Update intro1.rs

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -21,4 +21,5 @@ fn main() {
     println!("The file of this exercise is `exercises/00_intro/intro1.rs`. Have a look!");
     println!("The current exercise path will be always shown under the progress bar.");
     println!("You can click on the path to open the exercise file in your editor.");
+    println!("Hello.");
 }
