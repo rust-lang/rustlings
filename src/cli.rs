23f490d41ca94704d26b23935bd3e3efcde03b4e
manmen-mi <manmen.mi1375@gmail.com>
2026-05-24 14:10:02 +0900

    add args

--- src/cli.rs
+++ src/cli.rs
@@ -26,6 +26,9 @@ pub struct Args {
     /// Only use this if Rustlings fails to detect exercise file changes
     #[arg(long)]
     pub manual_run: bool,
+    /// Change rustbook url to passed one.
+    #[arg(long)]
+    pub base_url: Option<String>,
 }
 
 #[derive(Subcommand)]
