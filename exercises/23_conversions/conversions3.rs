11ce37fc937429363bd8cc410a6bd19d2e90b5b9
sijun-yang <yangsijun5528@gmail.com>
2026-08-09 15:35:18 +0900

    Preserve conversions3 wording

--- exercises/23_conversions/conversions3.rs
+++ exercises/23_conversions/conversions3.rs
@@ -1,7 +1,7 @@
 // In this exercise, we'll implement `FromStr` and return errors instead of
-// falling back to a default value. Upon implementing `FromStr`, you can use the
-// `parse` method on strings to generate an object of the implementor type.
-// You can read more about it in the documentation:
+// falling back to a default value. Additionally, upon implementing `FromStr`,
+// you can use the `parse` method on strings to generate an object of the
+// implementor type. You can read more about it in the documentation:
 // https://doc.rust-lang.org/std/str/trait.FromStr.html
 
 use std::num::ParseIntError;
